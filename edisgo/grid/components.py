e4012984f1dff142319273ac6c2695322bfeda27
gplessm <guido.plessmann@rl-institut.de>
2017-07-10 18:55:31 +0200

    Add Generator class definition

--- edisgo/grid/components.py
+++ edisgo/grid/components.py
@@ -8,3 +8,13 @@ class Load:
         self._consumption = kwargs.get('consumption', None)
 
 
+class Generator:
+    """Generator object"""
+
+    def __init__(self, **kwargs):
+        self._id = kwargs.get('id', None)
+        self._geom = kwargs.get('geom', None)
+        self._nominal_capacity = kwargs.get('nominal_capacity', None)
+        self._type = kwargs.get('type', None)
+        self._subtype = kwargs.get('subtype', None)
+        self._timeseries = kwargs.get('timeseries', None)
