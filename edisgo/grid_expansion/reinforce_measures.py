9acaceb98aababfdc451d3230831e0852e3a15cd
Birgit Schachler <RL-INSTITUT\birgit.schachler@solydxk>
2017-08-16 14:26:10 +0200

    First draft of method for reinforcing branches due to voltage problems

--- edisgo/grid_expansion/reinforce_measures.py
+++ edisgo/grid_expansion/reinforce_measures.py
@@ -1,9 +1,13 @@
 import copy
 import math
 import pandas as pd
+import networkx as nx
+from networkx.algorithms.shortest_paths.weighted import _dijkstra as \
+    dijkstra_shortest_path_length
 
 import ding0
 from edisgo.grid.components import Transformer
+from edisgo import grid_expansion
 # from ding0.tools import config as cfg_ding0
 # from ding0.grid.lv_grid.build_grid import select_transformers
 # from ding0.flexopt.check_tech_constraints import get_voltage_at_bus_bar
@@ -101,6 +105,114 @@ def extend_distribution_substation(critical_stations):
                 "issues.".format(stations_cnt=len(critical_stations)))
 
 
+def reinforce_branches_voltage(grid, crit_nodes):
+    """ Reinforce MV or LV grid by installing a new branch/line type
+
+    Parameters
+    ----------
+    grid: Grid object
+    crit_nodes: List of nodes objects with critical voltages sorted by voltage
+                (descending)
+
+    Notes
+    -----
+    Reinforce measures:
+    1. Disconnect line at 2/3 of the length between station and critical node
+    farthest away from the station and install new standard line
+    2. Install parallel standard line
+
+    References
+    ----------
+    .. [1] "Verteilnetzstudie für das Land Baden-Württemberg"
+    """
+
+    # ToDo: gilt Methodik auch für die MS?
+
+    # load cable data, file_names and parameter
+    # branch_parameters = grid.network.static_data['{gridlevel}_cables'.format(
+    #     gridlevel=grid_level)]
+    # branch_parameters = branch_parameters[
+    #     branch_parameters['U_n'] == grid.v_level].sort_values('I_max_th')
+    # ToDo: get parameters from config
+    standard_line = pd.Series({
+        'U_n': 400, 'I_max_th': 270, 'R': 0.1, 'L': 0.28, 'C': None},
+         name='NAYY 4x150')
+
+    # find first nodes of every main line as representatives
+    rep_main_line = nx.predecessor(grid._graph, grid.station, cutoff=1)
+    # list containing all representatives of main lines that have already been
+    # reinforced
+    main_line_reinforced = []
+
+    for crit_node in crit_nodes:
+        path = nx.shortest_path(grid._graph, grid.station, crit_node)
+
+        # check if representative of line is already in list
+        # main_line_reinforced, if it is the main line the critical node is
+        # connected to has already been reinforced in this iteration step
+        if not path[1] in main_line_reinforced:
+
+            main_line_reinforced.append(path[1])
+            # get path length from station to critical node
+            get_weight = lambda u, v, data: data['line']._length
+            path_length = dijkstra_shortest_path_length(
+                grid._graph, grid.station, get_weight, target=crit_node)
+            # find first node in path that exceeds 2/3 of the line length
+            # from station to critical node farthest away from the station
+            node_2_3 = next(i for i in path if
+                              path_length[i] >= path_length[crit_node] * 2 / 3)
+
+            # if node_2_3 is a representative (meaning it is already directly
+            # connected to the station), line cannot be disconnected and must
+            # therefore be reinforced
+            if node_2_3 in rep_main_line:
+                crit_line = grid._graph.get_edge_data(
+                    grid.station, node_2_3)['line']
+
+                # if critical line is already a standard line install one more
+                # parallel line
+                if crit_line._type.name == 'NAYY 4x150':
+                    # ToDo: increment number of parallel lines
+                    pass
+
+                # if critical line is not yet a standard line check if one or
+                # several standard lines are needed
+                else:
+                    # check if new standard line might solve the voltage
+                    # problem
+                    # ToDo: welche Kenngröße verwenden?
+                    if crit_line._type['I_max_th'] < standard_line['I_max_th']:
+                        crit_line._type = standard_line.copy()
+                    else:
+                        # ToDo: wie viele Standardbetriebsmittel?
+                        crit_line._type = standard_line.copy()
+
+            # if node_2_3 is not a representative, disconnect line
+            else:
+                # get line between node_2_3 and predecessor node (that is
+                # closer to the station)
+                pred_node = path[path.index(node_2_3) - 1]
+                crit_line = grid._graph.get_edge_data(
+                    node_2_3, pred_node)['line']
+                # add new edge between node_2_3 and station
+                new_line_data = {'line': crit_line,
+                                 'type': 'line'}
+                grid._graph.add_edge(grid.station, node_2_3, new_line_data)
+                # remove old edge
+                grid._graph.remove_edge(pred_node, node_2_3)
+                # change line length and type
+                crit_line._length = path_length[node_2_3]
+                crit_line._type = standard_line.copy()
+
+        else:
+            logger.debug('==> {} branches were reinforced.'.format(
+                str(len(main_line_reinforced))))
+
+    if main_line_reinforced:
+        logger.info('==> {} branches were reinforced.'.format(
+            str(len(main_line_reinforced))))
+
+
 def reinforce_branches_current(grid, crit_branches):
     """ Reinforce MV or LV grid by installing a new branch/line type
     
@@ -144,50 +256,6 @@ def reinforce_branches_current(grid, crit_branches):
     #                        'available. Original type is retained.'.format(
     #             branch))
     #         pass
-    #
-    # if branch_ctr:
-    #     logger.info('==> {} branches were reinforced.'.format(str(branch_ctr)))
-
-
-def reinforce_branches_voltage(grid, crit_branches, grid_level='MV'):
-    """ Reinforce MV or LV grid by installing a new branch/line type
-
-    Parameters
-    ----------
-    grid: GridDing0 object
-    crit_branches: List of BranchDing0 objects
-        list of critical branches
-
-    Notes
-    -----
-    The branch type to be installed is determined per branch - the next larger cable
-    available is used. According to [1]_ only cables are installed.
-
-    References
-    ----------
-    .. [1] Ackermann et al. (RP VNS)
-    """
-
-    # # load cable data, file_names and parameter
-    # branch_parameters = grid.network.static_data['{gridlevel}_cables'.format(
-    #     gridlevel=grid_level)]
-    # branch_parameters = branch_parameters[branch_parameters['U_n'] == grid.v_level].sort_values('I_max_th')
-    #
-    # branch_ctr = 0
-    #
-    # for branch in crit_branches:
-    #     try:
-    #         type = branch_parameters.ix[branch_parameters.loc[branch_parameters['I_max_th'] >
-    #                                     branch.type['I_max_th']]['I_max_th'].idxmin()]
-    #         branch.type = type
-    #         branch_ctr += 1
-    #     except:
-    #         logger.warning('Branch {} could not be reinforced (voltage '
-    #                        'issues) as there is no appropriate cable type '
-    #                        'available. Original type is retained.'.format(
-    #             branch))
-    #         pass
-    #
     #
     # if branch_ctr:
     #     logger.info('==> {} branches were reinforced.'.format(str(branch_ctr)))
