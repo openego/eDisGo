c343f1f64189fc9d69198f13d209ed5e0b70906f
gplessm <guido.plessmann@rl-institut.de>
2017-09-01 21:58:28 +0200

    Fix example prints

--- edisgo/examples/example.py
+++ edisgo/examples/example.py
@@ -13,7 +13,10 @@ for gen in gens:
 
 # Load located in aggregated LAs
 print('\n\nAggregated load in LA adds up to\n')
-[print('\t{0}: {1} MWh'.format(
-    _,
-    network.mv_grid.graph.nodes_by_attribute('load')[0].consumption[_] / 1e3))
-    for _ in ['retail', 'industrial', 'agricultural', 'residential']]
+if network.mv_grid.graph.nodes_by_attribute('load'):
+    [print('\t{0}: {1} MWh'.format(
+        _,
+        network.mv_grid.graph.nodes_by_attribute('load')[0].consumption[_] / 1e3))
+        for _ in ['retail', 'industrial', 'agricultural', 'residential']]
+else:
+    print("O MWh")
