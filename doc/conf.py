c129f6ea9a111187c6c238c4fa223d08313d8a39
gplessm <guido.plessmann@rl-institut.de>
2017-09-04 19:41:28 +0200

    Include ding0 and mock shapely

--- doc/conf.py
+++ doc/conf.py
@@ -162,7 +162,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['ding0']
+MOCK_MODULES = ['shapely']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
