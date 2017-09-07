bdc4ce1012179b5f6c98384d11cce33bd5af2ef9
birgit <birgit.schachler@rl-institut.de>
2017-09-07 11:42:37 +0200

    Adapt example to new results structure

--- edisgo/examples/example.py
+++ edisgo/examples/example.py
@@ -5,17 +5,18 @@ import pickle
 import pandas as pd
 from ast import literal_eval
 import numpy as np
+import datetime
 
 timeseries = TimeSeries()
 scenario = Scenario(timeseries=timeseries)
 
-network = Network.import_from_ding0(
-    os.path.join('data', 'ding0_grids__3545.pkl'),
-    id='Test grid',
-    scenario=scenario
-)
+# network = Network.import_from_ding0(
+#     os.path.join('data', 'ding0_grids_example.pkl'),
+#     id='Test grid',
+#     scenario=scenario
+# )
 # pickle.dump(network, open('test_network.pkl', 'wb'))
-# network = pickle.load(open('test_network.pkl', 'rb'))
+network = pickle.load(open('test_network.pkl', 'rb'))
 
 # export to pypsa
 # network.analyze(mode='mv')
@@ -23,20 +24,32 @@ network = Network.import_from_ding0(
 # for now create results object
 # ToDo: Werte in DataFrame als List oder Array?
 results = Results()
-results.pfa_edges = pd.read_csv('Exemplary_PyPSA_line_results.csv',
-                                index_col=0,
-                                converters={'p0': literal_eval,
-                                            'q0': literal_eval,
-                                            'p1': literal_eval,
-                                            'q1': literal_eval})
-results.pfa_edges['p0'] = results.pfa_edges['p0'].apply(lambda x: np.array(x))
-results.pfa_edges['q0'] = results.pfa_edges['q0'].apply(lambda x: np.array(x))
-results.pfa_edges['p1'] = results.pfa_edges['p1'].apply(lambda x: np.array(x))
-results.pfa_edges['q1'] = results.pfa_edges['q1'].apply(lambda x: np.array(x))
-results.pfa_nodes = pd.read_csv('Exemplary_PyPSA_bus_results.csv', index_col=0,
-                                converters={'v_mag_pu': literal_eval})
-results.pfa_nodes['v_mag_pu'] = results.pfa_nodes['v_mag_pu'].apply(
-    lambda x: np.array(x))
+pfa_edges = pd.read_csv('Exemplary_PyPSA_line_results.csv',
+                        index_col=0,
+                        converters={'p0': literal_eval,
+                                    'q0': literal_eval,
+                                    'p1': literal_eval,
+                                    'q1': literal_eval})
+
+results.pfa_p['p0'] = pfa_edges['p0'].apply(
+    lambda x: np.array(x)[0])
+results.pfa_p = results.pfa_p.transpose()
+results.pfa_p['time'] = datetime.date.today()
+results.pfa_p = results.pfa_p.set_index('time')
+
+results.pfa_q['q0'] = pfa_edges['q0'].apply(
+    lambda x: np.array(x)[0])
+results.pfa_q = results.pfa_q.transpose()
+results.pfa_q['time'] = datetime.date.today()
+results.pfa_q = results.pfa_q.set_index('time')
+
+pfa_nodes = pd.read_csv('Exemplary_PyPSA_bus_results.csv', index_col=0,
+                        converters={'v_mag_pu': literal_eval})
+results.pfa_v_mag_pu['v_mag_pu'] = pfa_nodes['v_mag_pu'].apply(
+    lambda x: np.array(x)[0])
+results.pfa_v_mag_pu = results.pfa_v_mag_pu.transpose()
+results.pfa_v_mag_pu['time'] = datetime.date.today()
+results.pfa_v_mag_pu = results.pfa_v_mag_pu.set_index('time')
 
 # # MV generators
 # gens = network.mv_grid.graph.nodes_by_attribute('generator')
@@ -63,8 +76,6 @@ reinforce_grid.reinforce_grid(network, results)
 
 # nx.draw_spectral(list(network.mv_grid.lv_grids)[0].graph)
 
-# ToDo: wie halten wir fest, welche Betriebsmittel erneuert wurden, um hinterher Kosten berechnen zu können?
 # ToDo: Parameter bei Komponenten einführen mit dem man feststellen kann, ob die Komponente bereits in einer ersten Maßnahme verstärkt oder ausgebaut wurde
-# ToDo: config mit Standardbetriebsmitteln?
 # ToDo: Abbruchkriterium einführen - Anzahl paralleler lines
 
