d194667d5a49cdda05bc74baeb94350851a7da0f
nesnoj <jonathan.amme@rl-institut.de>
2017-10-16 18:05:39 +0200

    add geno import to example file

--- edisgo/examples/example.py
+++ edisgo/examples/example.py
@@ -47,6 +47,8 @@ if __name__ == '__main__':
             id='Test grid',
             scenario=scenario)
         try:
+            # Import generators
+            network.import_generators()
             # Do non-linear power flow analysis with PyPSA
             network.analyze()
             # Do grid reinforcement
