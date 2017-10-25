4969ab8348d54e90000d156ae21c398e6a9a3c50
Birgit Schachler <birgit.schachler@rl-institut.de>
2017-10-25 13:50:08 +0200

    Bug fix rename hv/mv transformer attributes s, x and r

--- edisgo/data/import_data.py
+++ edisgo/data/import_data.py
@@ -321,7 +321,7 @@ def _build_mv_grid(ding0_grid, network):
             geom=ding0_grid.station().geo_data,
             voltage_op=_.v_level,
             type=pd.Series(dict(
-                s=_.s_max_a, x=_.x, r=_.r)))
+                S_nom=_.s_max_a, X=_.x, R=_.r)))
             for (count, _) in enumerate(ding0_grid.station().transformers())])
     grid.graph.add_node(mv_station, type='mv_station')
 
