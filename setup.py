c86a7d796808cddf83d6484da30e709e4b7fe27b
gplessm <guido.plessmann@rl-institut.de>
2018-01-17 22:35:11 +0100

    Update version in setup.py to next one

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class InstallSetup(install):
 
 setup(
     name='eDisGo',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/openego/eDisGo',
     license='GNU Affero General Public License v3.0',
