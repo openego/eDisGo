e8ff703c68a27b7aa1c145a7ef66a35a7fdddf12
birgit_s <birgit.schachler@rl-institut.de>
2018-07-09 09:52:01 +0200

    Clean up storage positioning for now

--- edisgo/flex_opt/storage_positioning.py
+++ edisgo/flex_opt/storage_positioning.py
@@ -2,11 +2,13 @@ import pandas as pd
 import networkx as nx
 from math import sqrt
 from edisgo.grid.grids import LVGrid
-from edisgo.grid.components import LVStation
+from edisgo.grid.components import LVStation, Transformer
 from edisgo.flex_opt import storage_integration
 
 
 def one_storage_per_feeder(edisgo, storage_parameters):
+    # ToDo: add parameters from etrago specs
+    # ToDo: document
 
     def feeder_ranking(grid_expansion_costs):
         """
@@ -30,52 +32,49 @@ def one_storage_per_feeder(edisgo, storage_parameters):
             reset_index().sort_values(by=['total_costs'], ascending=False)[
             'mv_feeder']
 
-    # rank MV feeders by grid expansion costs
-    if edisgo.network.results.grid_expansion_costs is not None:
-        grid_expansion_results = edisgo.network.results
-    else:
-        # conduct grid reinforcement
-        #grid_expansion_results = edisgo.reinforce(copy_graph=True)
-        import pickle
-        edisgo_reinforce = pickle.load(open('edisgo.pkl', 'rb'))
-        grid_expansion_results = edisgo_reinforce.network.results
+    def find_battery_node(components):
+        """
+        Evaluates where to install the storage
 
-    # rank feeders
-    ranked_feeders = feeder_ranking(
-        grid_expansion_results.grid_expansion_costs)
+        Returns
+        --------
+        Node where storage is installed.
 
-    for feeder in ranked_feeders.values:
-        ### install storage at the node of the overloaded lines farthest away
-        ### from the HV/MV transformer
-
-        # get the nodes farthest away from HV/MV transformer
-        lines = grid_expansion_results.grid_expansion_costs.loc[
-            grid_expansion_results.grid_expansion_costs.mv_feeder==feeder].\
-            index
+        """
+        # nodes_mv will contain all nodes of MV lines with over-loading or
+        # over-voltage issues; lv_stations will contain all stations of LV
+        # grids with over-loading or over-voltage issues
         nodes_mv = []
         lv_stations = []
-        for line in lines:
-            if isinstance(line.grid, LVGrid):
-                lv_stations.append(line.grid.station)
+        for comp in components:
+            if isinstance(comp, Transformer) or isinstance(comp.grid, LVGrid):
+                lv_stations.append(comp.grid.station)
             else:
-                nodes_mv.extend(list(line.grid.graph.nodes_from_line(line)))
-        # if there are nodes with issues in the MV grid find node farthest away from station
+                nodes_mv.extend(list(comp.grid.graph.nodes_from_line(comp)))
+
+        # if there are nodes with issues in the MV grid the battery storage
+        # will be installed at the node farthest away from the MV station
         if nodes_mv:
-            # dictionary with path length as key and corresponding node as value
+            # dictionary with nodes and their corresponding path length to
+            # MV station
             path_length_dict = {}
             for node in nodes_mv:
                 if isinstance(node, LVStation):
-                    path_length_dict[node] = len(nx.shortest_path(node.mv_grid.graph, node.mv_grid.station, node))
+                    path_length_dict[node] = len(
+                        nx.shortest_path(node.mv_grid.graph,
+                                         node.mv_grid.station, node))
                 else:
-                    path_length_dict[node] = len(nx.shortest_path(node.grid.graph, node.grid.station, node))
+                    path_length_dict[node] = len(
+                        nx.shortest_path(node.grid.graph, node.grid.station,
+                                         node))
             # find node farthest away
             battery_node = [_ for _ in path_length_dict.keys()
-                            if path_length_dict[_]==
+                            if path_length_dict[_] ==
                             max(path_length_dict.values())][0]
-        # if there are only issues in the LV grids find LV station with issues closest to
-        # MV station
+        # if there are only issues in the LV grids find LV station with issues
+        # closest to MV station
         else:
-            # dictionary with path lengths
+            # calculate path lengths
             path_length_dict = {}
             for node in lv_stations:
                 path_length_dict[node] = len(nx.shortest_path(
@@ -84,40 +83,58 @@ def one_storage_per_feeder(edisgo, storage_parameters):
             battery_node = [_ for _ in path_length_dict.keys()
                             if path_length_dict[_] ==
                             min(path_length_dict.values())][0]
+        # ToDo: Return node in original edisgo object instead of copied one
+        return battery_node
 
-        ### estimate residual line load (neglect grid losses) to identify all time steps where line is over-loaded
-        # get line
-        line = None
-        counter = 0
-        while line is None:
-            if battery_node in lines[counter].grid.graph.nodes_from_line(lines[counter]):
-                line = lines[counter]
-            counter += 1
-        # analyze
-        edisgo.analyze()
+    # rank MV feeders by grid expansion costs
+    # conduct grid reinforcement on copied edisgo object on worst-case time
+    # steps
+    # ToDo: reinforce for worst-case once implemented
+    grid_expansion_results = edisgo.reinforce(copy_graph=True)
+    # ToDo: delete once implementation is finished
+    # import pickle
+    # edisgo_reinforce = pickle.load(open('edisgo.pkl', 'rb'))
+    # grid_expansion_results = edisgo_reinforce.network.results
+    ranked_feeders = feeder_ranking(
+        grid_expansion_results.grid_expansion_costs)
+
+    for feeder in ranked_feeders.values:
+
+        # first step: find node where storage will be installed
+        # get all reinforced components in respective feeder
+        components = grid_expansion_results.grid_expansion_costs.loc[
+            grid_expansion_results.grid_expansion_costs.mv_feeder == feeder]. \
+            index
+        battery_node = find_battery_node(components)
+
+        # second step: estimate residual line load to calculate size of battery
+        # ToDo: find battery line in original edisgo object instead of copy
+        path_to_battery_node = nx.shortest_path(
+            feeder.grid.graph, feeder.grid.station, battery_node)
+        battery_line = feeder.grid.graph.line_from_nodes(
+            path_to_battery_node[-1], path_to_battery_node[-2])
+        # analyze (probably not needed here since first analyze in conducted
+        # in reinforce_grid) for every time step
+        #edisgo.analyze()
+
+        # allowed line load
         # ToDo: Differentiate between load and feedin case
-        load_factor_line = edisgo.network.config['grid_expansion_load_factors'][
-            'mv_feedin_case_line']
-        i_allowed = line.type['I_max_th'] * load_factor_line * line.quantity
-        u_allowed = line.type['U_n']
-        i_pf = edisgo.network.results.i_res[repr(line)]
-
-        # to calculate max p of line before last reinforcement
-        S_ol_line_nom = sqrt(3) * i_allowed * u_allowed  # rated apparent power S of line kVA
+        load_factor_line = edisgo.network.config[
+            'grid_expansion_load_factors']['mv_feedin_case_line']
+        i_allowed = battery_line.type['I_max_th'] * load_factor_line * \
+                    battery_line.quantity
+        u_allowed = battery_line.type['U_n']
+        # ToDo: correct?
+        s_allowed = sqrt(3) * i_allowed * u_allowed
 
         # maximum power of the line
-        pfa_q = edisgo.network.results.pfa_q[repr(line)]
+        pfa_q = edisgo.network.results.pfa_q[repr(battery_line)]
         # ToDo: storage efficiency
-        p_max = sqrt(S_ol_line_nom ** 2 - pfa_q ** 2)
+        p_max = sqrt(s_allowed ** 2 - pfa_q ** 2)
 
-        # integrate storage
+        # third step: integrate storage
         storage_parameters['nominal_capacity'] = p_max
         storage_integration.set_up_storage(storage_parameters, battery_node)
 
-
-
-    # for every feeder:
-    # ** install storage at the node of the overloaded lines farthest away from the HV/MV transformer
-    # ** estimate residual line load (neglect grid losses) to identify all time steps where line is over-loaded
-    # ** determine storage capacity by adding storage dispatch time series to residual line load and checking if line load can be reduced; maximum needed reduction to avoid all line over-loadings determines the storage capacity
-    # ** if there is storage capacity left, rerun the past steps for the next feeder in the ranking list
+        # fourth step: if there is storage capacity left, rerun the past steps
+        # for the next feeder in the ranking list
