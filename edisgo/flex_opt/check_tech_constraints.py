e6177d23e065f36d1cf841c821deb1c6d0fabf9a
birgit_s <birgit.schachler@rl-institut.de>
2018-07-13 00:15:15 +0200

    Consider load and feed-in case for when checking over-loading of stations

--- edisgo/flex_opt/check_tech_constraints.py
+++ edisgo/flex_opt/check_tech_constraints.py
@@ -2,6 +2,7 @@ import pandas as pd
 import logging
 
 from edisgo.grid.grids import LVGrid
+from edisgo.grid.components import LVStation
 
 logger = logging.getLogger('edisgo')
 
@@ -88,7 +89,7 @@ def lv_line_load(network):
 
 def _line_load(network, grid, crit_lines):
     """
-    Checks for over-loading issues in MV grid.
+    Checks for over-loading issues of lines.
 
     Parameters
     ----------
@@ -162,32 +163,26 @@ def hv_mv_station_load(network):
 
     Returns
     -------
-    Dictionary with critical :class:`~.grid.components.MVStation` and maximum
-    apparent power from power flow analysis. Format: {mv_station: S_max}
+    :pandas:`pandas.DataFrame<dataframe>`
+        Dataframe containing over-loaded HV/MV stations, their apparent power
+        at maximal over-loading and the corresponding time step.
+        Index of the dataframe are the over-loaded stations of type
+        :class:`~.grid.components.MVStation`. Columns are 's_pfa'
+        containing the apparent power at maximal over-loading as float and
+        'time_index' containing the corresponding time step the over-loading
+        occured in as :pandas:`pandas.Timestamp<timestamp>`.
+
+    Notes
+    -----
+    Over-load is determined based on allowed load factors for feed-in and
+    load cases that are defined in the config file 'config_grid_expansion' in
+    section 'grid_expansion_load_factors'.
 
     """
-
-    crit_stations = {}
-
-    #ToDo: differentiate between load and feed-in case!
-    load_factor = network.config['grid_expansion_load_factors'][
-        'mv_feedin_case_transformer']
-
-    # maximum allowed apparent power of station
-    s_station_max = (sum(
-        [_.type.S_nom for _ in network.mv_grid.station.transformers]) *
-                     load_factor)
-    try:
-        # check if maximum allowed apparent power of station exceeds
-        # apparent power from power flow analysis
-        s_station_pfa = network.results.s_res(
-            [network.mv_grid.station]).sum(axis=1).max()
-        if s_station_max < s_station_pfa:
-            crit_stations[network.mv_grid.station] = s_station_pfa
-    except KeyError:
-        logger.debug('No results for MV station to check overloading.')
-
-    if crit_stations:
+    crit_stations = pd.DataFrame()
+    crit_stations = _station_load(network, network.mv_grid.station,
+                                  crit_stations)
+    if not crit_stations.empty:
         logger.debug('==> HV/MV station has load issues.')
     else:
         logger.debug('==> No HV/MV station load issues.')
@@ -205,46 +200,111 @@ def mv_lv_station_load(network):
 
     Returns
     -------
-    Dictionary with critical :class:`~.grid.components.LVStation` and maximum
-    apparent power from power flow analysis.
-    Format: {lv_station_1: S_max_1, ..., lv_station_n: S_max_n}
+    :pandas:`pandas.DataFrame<dataframe>`
+        Dataframe containing over-loaded MV/LV stations, their apparent power
+        at maximal over-loading and the corresponding time step.
+        Index of the dataframe are the over-loaded stations of type
+        :class:`~.grid.components.LVStation`. Columns are 's_pfa'
+        containing the apparent power at maximal over-loading as float and
+        'time_index' containing the corresponding time step the over-loading
+        occured in as :pandas:`pandas.Timestamp<timestamp>`.
 
     Notes
     -----
-    According to [VerteilnetzstudieBW]_ load factors in feed-in case of all equipment in MV and
-    LV is set to 1.
-
-    HV/MV transformers are not checked.
+    Over-load is determined based on allowed load factors for feed-in and
+    load cases that are defined in the config file 'config_grid_expansion' in
+    section 'grid_expansion_load_factors'.
 
     """
 
-    crit_stations = {}
-
-    # ToDo: differentiate between load and feed-in case!
-    load_factor = network.config['grid_expansion_load_factors'][
-        'lv_feedin_case_transformer']
+    crit_stations = pd.DataFrame()
 
     for lv_grid in network.mv_grid.lv_grids:
-        station = lv_grid.station
-        # maximum allowed apparent power of station
-        s_station_max = (sum([_.type.S_nom for _ in station.transformers]) *
-                         load_factor)
-        try:
-            # check if maximum allowed apparent power of station exceeds
-            # apparent power from power flow analysis
-            s_station_pfa = max(network.results.s_res(
-                station.transformers).sum(axis=1))
-            if s_station_max < s_station_pfa:
-                crit_stations[station] = s_station_pfa
-        except KeyError:
-            logger.debug('No results for LV station {} '.format(str(station)) +
-                         'to check overloading.')
-
-    if crit_stations:
+        crit_stations = _station_load(network, lv_grid.station,
+                                      crit_stations)
+    if not crit_stations.empty:
         logger.debug('==> {} MV/LV station(s) has/have load issues.'.format(
-            len(crit_stations)))
+            crit_stations.shape[0]))
     else:
         logger.debug('==> No MV/LV station load issues.')
+
+    return crit_stations
+
+
+def _station_load(network, station, crit_stations):
+    """
+    Checks for over-loading of stations.
+
+    Parameters
+    ----------
+    network : :class:`~.grid.network.Network`
+    station : :class:`~.grid.components.LVStation` or :class:`~.grid.components.MVStation`
+    crit_stations : :pandas:`pandas.DataFrame<dataframe>`
+        Dataframe containing over-loaded stations, their apparent power at
+        maximal over-loading and the corresponding time step.
+        Index of the dataframe are the over-loaded stations either of type
+        :class:`~.grid.components.LVStation` or
+        :class:`~.grid.components.MVStation`. Columns are 's_pfa'
+        containing the apparent power at maximal over-loading as float and
+        'time_index' containing the corresponding time step the over-loading
+        occured in as :pandas:`pandas.Timestamp<timestamp>`.
+
+    Returns
+    -------
+    :pandas:`pandas.DataFrame<dataframe>`
+        Dataframe containing over-loaded stations, their apparent power at
+        maximal over-loading and the corresponding time step.
+        Index of the dataframe are the over-loaded stations either of type
+        :class:`~.grid.components.LVStation` or
+        :class:`~.grid.components.MVStation`. Columns are 's_pfa'
+        containing the apparent power at maximal over-loading as float and
+        'time_index' containing the corresponding time step the over-loading
+        occured in as :pandas:`pandas.Timestamp<timestamp>`.
+
+    """
+    if isinstance(station, LVStation):
+        grid_level = 'lv'
+    else:
+        grid_level = 'mv'
+
+    # maximum allowed apparent power of station for feed-in and load case
+    s_station = sum(
+        [_.type.S_nom for _ in network.mv_grid.station.transformers])
+    s_station_allowed_per_case = {}
+    s_station_allowed_per_case['feedin_case'] = s_station * network.config[
+        'grid_expansion_load_factors']['{}_feedin_case_transformer'.format(
+        grid_level)]
+    s_station_allowed_per_case['load_case'] = s_station * network.config[
+        'grid_expansion_load_factors']['{}_load_case_transformer'.format(
+        grid_level)]
+    # maximum allowed apparent power of station in each time step
+    s_station_allowed = \
+        network.timeseries.timesteps_load_feedin_case.case.apply(
+            lambda _: s_station_allowed_per_case[_])
+
+    try:
+        s_station_pfa = network.results.s_res([network.mv_grid.station])
+        s_res = s_station_allowed - s_station_pfa.iloc[:, 0]
+        s_res = s_res[s_res < 0]
+        # check if maximum allowed apparent power of station exceeds
+        # apparent power from power flow analysis at any time step
+        if not s_res.empty:
+            # find out largest relative deviation
+            load_factor = \
+                network.timeseries.timesteps_load_feedin_case.case.apply(
+                    lambda _: network.config[
+                        'grid_expansion_load_factors'][
+                        '{}_{}_transformer'.format(grid_level, _)])
+            relative_s_res = load_factor * s_res
+            crit_stations = crit_stations.append(pd.DataFrame(
+                {'s_pfa': s_station_pfa.loc[relative_s_res.idxmin(),
+                                            repr(network.mv_grid.station)],
+                 'time_index': relative_s_res.idxmin()},
+                index=[network.mv_grid.station]))
+
+    except KeyError:
+        logger.debug('No results for {} station to check overloading.'.format(
+            grid_level.upper()))
 
     return crit_stations
 
