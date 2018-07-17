27f1a9fb5fd77d1c244d2c4c2ef88b2622fe21c5
birgit_s <birgit.schachler@rl-institut.de>
2018-07-17 13:08:59 +0200

    Bug fix typo

--- edisgo/tools/pypsa_io.py
+++ edisgo/tools/pypsa_io.py
@@ -546,7 +546,7 @@ def lv_to_pypsa(network):
         storage['name'].append(repr(sto))
         storage['bus'].append(bus_name)
         storage['p_nom'].append(sto.nominal_power)
-        storage['state_of_charge_initial'].append(sto.soc_inital)
+        storage['state_of_charge_initial'].append(sto.soc_initial)
         storage['efficiency_store'].append(sto.efficiency_in)
         storage['efficiency_dispatch'].append(sto.efficiency_out)
         storage['standing_loss'].append(sto.standing_loss)
