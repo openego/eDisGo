6b92a415cf21b7530c314b1827b314b3da0b2a0c
birgits <birgit.schachler@rl-institut.de>
2018-09-07 09:34:15 +0200

    Correct extlinks

--- doc/conf.py
+++ doc/conf.py
@@ -78,8 +78,8 @@ extlinks = {'pandas':('http://pandas.pydata.org/pandas-docs/stable/api.html#%s',
                       'SQLAlchemy session object'),
             'shapely':('http://toblerity.org/shapely/manual.html#%s',
                       'Shapely object'),
-            'dingo':('https://dingo.readthedocs.io/en/dev/api/dingo.html#%s',
-                     'Dingo'),
+            'ding0':('https://dingo.readthedocs.io/en/dev/api/ding0.flexopt.html#%s',
+                     'Ding0'),
             'pypsa': ('https://pypsa.org/doc/components.html#%s', 'pypsa')
             }
 
