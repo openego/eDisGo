55ecc5fc2ec786f5bbdd80929837f9c8b72559a5
birgits <birgit.schachler@rl-institut.de>
2018-10-13 20:24:19 +0200

    Add option to plot grid expansion costs

--- edisgo/tools/plots.py
+++ edisgo/tools/plots.py
@@ -354,8 +354,10 @@ def get_grid_district_polygon(config, subst_id=None, projection=4326):
     return region
 
 
-def line_loading(pypsa_network, configs, line_load, timestep,
-                 filename=None, arrows=False, node_color='technology',
+def line_loading(pypsa_network, configs, timestep=None,
+                 line_color=None, node_color=None,
+                 line_load=None, grid_expansion_costs=None,
+                 filename=None, arrows=False,
                  grid_district_geom=True, background_map=True,
                  voltage=None, limits_cb_load=None, limits_cb_voltage=None,
                  xlim=None, ylim=None):
@@ -370,24 +372,59 @@ def line_loading(pypsa_network, configs, line_load, timestep,
     configs : :obj:`dict`
         Dictionary with used configurations from config files. See
         :class:`~.grid.network.Config` for more information.
+    timestep : :pandas:`pandas.Timestamp<timestamp>`
+        Time step to plot analysis results for. If `timestep` is None maximum
+        line load and if given, maximum voltage deviation, is used. In that
+        case arrows cannot be drawn. Default: None.
+    line_color : :obj:`str`
+        Defines whereby to choose line colors (and implicitly size). Possible
+        options are:
+
+        * 'loading'
+          Line color is set according to loading of the line. Loading of MV
+          lines must be provided by parameter `line_load`.
+        * 'expansion_costs'
+          Line color is set according to investment costs of the line. This
+          option also effects node colors and sizes by plotting investment in
+          stations and setting `node_color` to 'storage_integration' in order
+          to plot storage size of integrated storages. Grid expansion costs
+          must be provided by parameter `grid_expansion_costs`.
+        * None (default)
+          Lines are plotted in black. Is also the fallback option in case of
+          wrong input.
+
+    node_color : :obj:`str`
+        Defines whereby to choose node colors (and implicitly size). Possible
+        options are:
+
+        * 'technology'
+          Node color as well as size is set according to type of node
+          (generator, MV station, etc.).
+        * 'voltage'
+          Node color is set according to voltage deviation. Voltages of nodes
+          in MV grid must be provided by parameter `voltage`.
+        * 'storage_integration'
+          Only storages are plotted. Size of node corresponds to size of
+          storage.
+        * None (default)
+          Nodes are not plotted. Is also the fallback option in case of wrong
+          input.
+
     line_load : :pandas:`pandas.DataFrame<dataframe>`
         Dataframe with current results from power flow analysis in A. Index of
         the dataframe is a :pandas:`pandas.DatetimeIndex<datetimeindex>`,
         columns are the line representatives.
-    timestep : :pandas:`pandas.Timestamp<timestamp>`
-        Time step to plot analysis results for. If `timestep` is None maximum
-        line load and if given, maximum voltage deviation, is used. In that
-        case arrows cannot be drawn.
+    grid_expansion_costs : :pandas:`pandas.DataFrame<dataframe>`
+        Dataframe with grid expansion costs in kEUR. See `grid_expansion_costs`
+        in :class:`~.grid.network.Results` for more information.
     filename : :obj:`str`
         Filename to save plot under. If not provided, figure is shown directly.
         Default: None.
     arrows : :obj:`Boolean`
-        If True draws arrows on lines in the direction of the power flow.
+        If True draws arrows on lines in the direction of the power flow. Does
+        only work when `line_color` option 'loading' is used and a time step
+        is given.
         Default: False.
-    node_color : :obj:`str`
-        Defines if colors of nodes are set by 'technology' or 'voltage'. If
-        'voltage' is chosen, voltages of nodes in MV grid must be provided by
-        parameter `voltage`. Default: 'technology'.
     limits_cb_load : :obj:`tuple`
         Tuple with limits for colorbar of line loading. First entry is the
         minimum and second entry the maximum value. Default: None.
@@ -467,39 +504,105 @@ def line_loading(pypsa_network, configs, line_load, timestep,
             bus_sizes[bus] = 50
         return bus_sizes, bus_colors
 
+    def nodes_storage_integration(buses):
+        bus_sizes = {}
+        for bus in buses:
+            if not 'storage' in bus:
+                bus_sizes[bus] = 0
+            else:
+                tmp = bus.split('_')
+                storage_repr = '_'.join(tmp[1:])
+                bus_sizes[bus] = pypsa_network.storage_units.loc[
+                           storage_repr, 'p_nom'] * 1000 / 3
+        return bus_sizes
+
+    def nodes_by_costs(buses, grid_expansion_costs):
+        # sum costs for each station
+        costs_lv_stations = grid_expansion_costs[
+            grid_expansion_costs.index.str.contains("LVStation")]
+        costs_lv_stations['station'] = \
+            costs_lv_stations.reset_index()['index'].apply(
+                lambda _: '_'.join(_.split('_')[0:2])).values
+        costs_lv_stations = costs_lv_stations.groupby('station').sum()
+        costs_mv_station = grid_expansion_costs[
+            grid_expansion_costs.index.str.contains("MVStation")]
+        costs_mv_station['station'] = \
+            costs_mv_station.reset_index()['index'].apply(
+                lambda _: '_'.join(_.split('_')[0:2])).values
+        costs_mv_station = costs_mv_station.groupby('station').sum()
+
+        bus_sizes = {}
+        bus_colors = {}
+        for bus in buses:
+            if 'LVStation' in bus:
+                try:
+                    tmp = bus.split('_')
+                    lv_st = '_'.join(tmp[2:])
+                    bus_colors[bus] = costs_lv_stations.loc[
+                        lv_st, 'total_costs']
+                    bus_sizes[bus] = 100
+                except:
+                    bus_colors[bus] = 0
+                    bus_sizes[bus] = 0
+            elif 'MVStation' in bus:
+                try:
+                    tmp = bus.split('_')
+                    mv_st = '_'.join(tmp[2:])
+                    bus_colors[bus] = costs_mv_station.loc[
+                        mv_st, 'total_costs']
+                    bus_sizes[bus] = 100
+                except:
+                    bus_colors[bus] = 0
+                    bus_sizes[bus] = 0
+            else:
+                bus_colors[bus] = 0
+                bus_sizes[bus] = 0
+
+        return bus_sizes, bus_colors
+
     # create pypsa network only containing MV buses and lines
     pypsa_plot = PyPSANetwork()
     pypsa_plot.buses = pypsa_network.buses.loc[pypsa_network.buses.v_nom >= 10]
     pypsa_plot.lines = pypsa_network.lines.loc[pypsa_network.lines.v_nom >= 10]
 
-    # calculate relative line loading
-    # get load factor
-    residual_load = tools.get_residual_load_from_pypsa_network(pypsa_network)
-    case = residual_load.apply(
-            lambda _: 'feedin_case' if _ < 0 else 'load_case')
-    if timestep is not None:
-        load_factor = float(configs['grid_expansion_load_factors'][
-            'mv_{}_line'.format(case.loc[timestep])])
-        # get allowed line load
-        i_line_allowed = pypsa_plot.lines.s_nom.divide(
-            pypsa_plot.lines.v_nom) / sqrt(3) * 1e3 * load_factor
-        # get line load from pf
-        i_line_pfa = line_load.loc[timestep,
-                                   pypsa_plot.lines.index]
-        loading = i_line_pfa.divide(i_line_allowed)
+    # line colors
+    if line_color == 'loading':
+        # calculate relative line loading
+        # get load factor
+        residual_load = tools.get_residual_load_from_pypsa_network(
+            pypsa_network)
+        case = residual_load.apply(
+                lambda _: 'feedin_case' if _ < 0 else 'load_case')
+        if timestep is not None:
+            load_factor = float(configs['grid_expansion_load_factors'][
+                'mv_{}_line'.format(case.loc[timestep])])
+            # get allowed line load
+            i_line_allowed = pypsa_plot.lines.s_nom.divide(
+                pypsa_plot.lines.v_nom) / sqrt(3) * 1e3 * load_factor
+            # get line load from pf
+            i_line_pfa = line_load.loc[timestep,
+                                       pypsa_plot.lines.index]
+            line_colors = i_line_pfa.divide(i_line_allowed)
+        else:
+            load_factor = pd.Series(
+                data=[float(configs['grid_expansion_load_factors'][
+                                    'mv_{}_line'.format(case.loc[_])])
+                      for _ in line_load.index],
+                index=line_load.index)
+            # get allowed line load
+            i_line_allowed = load_factor.to_frame().dot(
+                (pypsa_plot.lines.s_nom.divide(
+                    pypsa_plot.lines.v_nom) / sqrt(3) * 1e3).to_frame().T)
+            # get line load from pf
+            i_line_pfa = line_load.loc[:, pypsa_plot.lines.index]
+            line_colors = (i_line_pfa.divide(i_line_allowed)).max()
+    elif line_color == 'expansion_costs':
+        node_color = 'expansion_costs'
+        line_costs = pypsa_plot.lines.join(
+            grid_expansion_costs, rsuffix='costs', how='left')
+        line_colors = line_costs.total_costs.fillna(0)
     else:
-        load_factor = pd.Series(
-            data=[float(configs['grid_expansion_load_factors'][
-                                'mv_{}_line'.format(case.loc[_])])
-                  for _ in line_load.index],
-            index=line_load.index)
-        # get allowed line load
-        i_line_allowed = load_factor.to_frame().dot(
-            (pypsa_plot.lines.s_nom.divide(
-                pypsa_plot.lines.v_nom) / sqrt(3) * 1e3).to_frame().T)
-        # get line load from pf
-        i_line_pfa = line_load.loc[:, pypsa_plot.lines.index]
-        loading = (i_line_pfa.divide(i_line_allowed)).max()
+        line_colors = pd.Series('black', index=pypsa_plot.lines.index)
 
     # bus colors and sizes
     if node_color == 'technology':
@@ -509,6 +612,20 @@ def line_loading(pypsa_network, configs, line_load, timestep,
         bus_sizes, bus_colors = nodes_by_voltage(
             pypsa_plot.buses.index, voltage, configs)
         bus_cmap = plt.cm.Blues
+    elif node_color == 'storage_integration':
+        bus_sizes = nodes_storage_integration(pypsa_plot.buses.index)
+        bus_colors = 'orangered'
+        bus_cmap = None
+    elif node_color == 'expansion_costs':
+        bus_sizes, bus_colors = nodes_by_costs(pypsa_plot.buses.index,
+                                               grid_expansion_costs)
+        bus_cmap = None
+    else:
+        logging.warning('Choice for `node_color` is not valid. Default is '
+                        'used instead.')
+        bus_sizes = 0
+        bus_colors = 'r'
+        bus_cmap = None
 
     # convert bus coordinates to Mercator
     if contextily and background_map:
@@ -534,20 +651,32 @@ def line_loading(pypsa_network, configs, line_load, timestep,
                                            projection=projection)
         region.plot(ax=ax, color='white', alpha=0.2,
                     edgecolor='red', linewidth=2)
+
+    #ToDo set plot title corresponding to what was plotted
     cmap = plt.cm.get_cmap('inferno_r')
-    ll = pypsa_plot.plot(line_colors=loading, line_cmap=cmap, ax=ax,
-                         title="Line loading", line_widths=2,
+    ll = pypsa_plot.plot(line_colors=line_colors, line_cmap=cmap, ax=ax,
+                         title=None,
+                         line_widths=2, #pypsa_plot.lines.s_nom,
                          branch_components=['Line'], basemap=True,
                          bus_sizes=bus_sizes, bus_colors=bus_colors,
                          bus_cmap=bus_cmap)
 
     # color bar line loading
-    if limits_cb_load is None:
-        limits_cb_load = (min(loading), max(loading))
-    v = np.linspace(limits_cb_load[0], limits_cb_load[1], 101)
-    cb = plt.colorbar(ll[1], boundaries=v, ticks=v[0:101:10])
-    cb.set_clim(vmin=limits_cb_load[0], vmax=limits_cb_load[1])
-    cb.set_label('Line loading in %')
+    if line_color == 'loading':
+        if limits_cb_load is None:
+            limits_cb_load = (min(line_colors), max(line_colors))
+        v = np.linspace(limits_cb_load[0], limits_cb_load[1], 101)
+        cb = plt.colorbar(ll[1], boundaries=v, ticks=v[0:101:10])
+        cb.set_clim(vmin=limits_cb_load[0], vmax=limits_cb_load[1])
+        cb.set_label('Line loading in p.u.')
+    elif line_color == 'expansion_costs':
+        if limits_cb_load is None:
+            limits_cb_load = (min(min(line_colors), min(bus_colors.values())),
+                              max(max(line_colors), max(bus_colors.values())))
+        v = np.linspace(limits_cb_load[0], limits_cb_load[1], 101)
+        cb = plt.colorbar(ll[1], boundaries=v, ticks=v[0:101:10])
+        cb.set_clim(vmin=limits_cb_load[0], vmax=limits_cb_load[1])
+        cb.set_label('Grid expansion costs in kEUR')
 
     # color bar voltage
     if node_color == 'voltage':
@@ -562,6 +691,25 @@ def line_loading(pypsa_network, configs, line_load, timestep,
                             vmax=limits_cb_voltage[1])
         cb_voltage.set_label('Voltage deviation in p.u.')
 
+    # storages
+    if node_color == 'expansion_costs':
+        ax.scatter(
+            pypsa_plot.buses.loc[
+                pypsa_network.storage_units.loc[:, 'bus'], 'x'],
+            pypsa_plot.buses.loc[
+                pypsa_network.storage_units.loc[:, 'bus'], 'y'],
+            c='orangered',
+            s=pypsa_network.storage_units.loc[:, 'p_nom'] * 1000 / 3)
+    # add legend for storage size
+    if (node_color == 'storage_integration' or
+        node_color == 'expansion_costs') and \
+            pypsa_network.storage_units.loc[:, 'p_nom'].any() > 0:
+        scatter_handle = ax.scatter(
+            [], [], c='orangered', s=100, label='= 300 kW battery storage')
+        ax.legend(handles=[scatter_handle], scatterpoints=1, labelspacing=1,
+                  title='Storage size', borderpad=1.2, loc=2, framealpha=0.5,
+                  fontsize='medium')
+
     # axes limits
     if xlim is not None:
         ax.set_xlim(xlim[0], xlim[1])
@@ -573,12 +721,12 @@ def line_loading(pypsa_network, configs, line_load, timestep,
     ax.get_yaxis().set_visible(False)
 
     # draw arrows on lines
-    if arrows and timestep:
+    if arrows and timestep and line_color=='loading':
         path = ll[1].get_segments()
         colors = cmap(ll[1].get_array() / 100)
         for i in range(len(path)):
             if pypsa_network.lines_t.p0.loc[timestep,
-                                            loading.index[i]] > 0:
+                                            line_colors.index[i]] > 0:
                 arrowprops = dict(arrowstyle="->", color='b')#colors[i])
             else:
                 arrowprops = dict(arrowstyle="<-", color='b')#colors[i])
