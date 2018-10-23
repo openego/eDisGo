0ba941c36857e52ee73c404f2199c40f6bec0ffa
birgits <birgit.schachler@rl-institut.de>
2018-10-23 13:40:35 +0200

    Adapt example

--- edisgo/examples/example.py
+++ edisgo/examples/example.py
@@ -4,8 +4,14 @@
 This example shows the general usage of eDisGo. Grid expansion costs for
 distribution grids generated with ding0 are calculated assuming renewable
 and conventional power plant capacities as stated in the scenario framework of
-the German Grid Development Plan (Netzentwicklungsplan) for the year 2035
-and conducting a worst-case analysis.
+the German Grid Development Plan 2015 (Netzentwicklungsplan) for the year 2035
+(scenario B2). To determine grid expansion needs worst-case scenarios (heavy
+load flow and reverse power flow) used in conventional grid expansion planning
+are set up.
+
+The example assumes you have ding0 grids in the current working directory. If
+you need more information on how to get ding0 grids see the ding0 documentation
+or the Quickstart section of the eDisGo documentation.
 
 As the grids generated with ding0 should represent current stable grids but
 have in some cases stability issues, grid expansion is first conducted before
@@ -35,21 +41,17 @@ logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
 
-    # get filenames of all pickled ding0 grids in directory
-    # 'edisgo/examples/data/'
+    # get filenames of all pickled ding0 grids in current working directory
     grids = []
-    for file in os.listdir(os.path.join(sys.path[0], "data")):
+    for file in os.listdir(sys.path[0]):
         if file.endswith(".pkl"):
             grids.append(file)
 
     # set scenario to define future power plant capacities
     scenario = 'nep2035'
 
-    # initialize containers that will hold grid expansion costs and, in the
-    # case of any errors during the run, the error messages
-    costs_before_geno_import = pd.DataFrame()
+    # initialize containers that will hold any error messages
     faulty_grids_before_geno_import = {'grid': [], 'msg': []}
-    costs = pd.DataFrame()
     faulty_grids = {'grid': [], 'msg': []}
 
     for dingo_grid in grids:
@@ -64,20 +66,30 @@ if __name__ == '__main__':
             # Calculate grid expansion costs before generator import
             logging.info('Grid expansion before generator import.')
             before_geno_import = True
+
+            # overwrite config parameters for allowed voltage deviations in
+            # initial grid reinforcement (status quo)
+            edisgo.network.config[
+                'grid_expansion_allowed_voltage_deviations'] = {
+                'hv_mv_trafo_offset': 0.04,
+                'hv_mv_trafo_control_deviation': 0.0,
+                'mv_load_case_max_v_deviation': 0.055,
+                'mv_feedin_case_max_v_deviation': 0.02,
+                'lv_load_case_max_v_deviation': 0.065,
+                'lv_feedin_case_max_v_deviation': 0.03,
+                'mv_lv_station_load_case_max_v_deviation': 0.02,
+                'mv_lv_station_feedin_case_max_v_deviation': 0.01
+            }
             # Do grid reinforcement
             edisgo.reinforce()
-            # Get costs
-            costs_grouped = \
-                edisgo.network.results.grid_expansion_costs.groupby(
-                    ['type']).sum()
-            costs_before_geno_import = costs_before_geno_import.append(
-                pd.DataFrame(costs_grouped.values,
-                             columns=costs_grouped.columns,
-                             index=[[edisgo.network.id] * len(costs_grouped),
-                                    costs_grouped.index]))
+            # Save results
+            edisgo.network.results.save(
+                'results_grid_{}_before_generator_import'.format(
+                    edisgo.network.id))
 
-            # Clear results
+            # Clear results and reset configs
             edisgo.network.results = Results(edisgo.network)
+            edisgo.network.config = None
 
             # Calculate grid expansion costs after generator import
             logging.info('Grid expansion after generator import.')
@@ -88,14 +100,10 @@ if __name__ == '__main__':
 
             # Do grid reinforcement
             edisgo.reinforce()
-            costs_grouped = \
-                edisgo.network.results.grid_expansion_costs.groupby(
-                    ['type']).sum()
-            costs = costs.append(
-                pd.DataFrame(costs_grouped.values,
-                             columns=costs_grouped.columns,
-                             index=[[edisgo.network.id] * len(costs_grouped),
-                                    costs_grouped.index]))
+            # Save results
+            edisgo.network.results.save('results_grid_{}'.format(
+                edisgo.network.id))
+
             logging.info('SUCCESS!')
 
         except MaximumIterationError:
@@ -120,15 +128,7 @@ if __name__ == '__main__':
                 faulty_grids['msg'].append(repr(e))
             logging.info('Something went wrong.')
 
-    # write costs and error messages to csv files
+    # write error messages to csv files
     pd.DataFrame(faulty_grids_before_geno_import).to_csv(
         'faulty_grids_before_geno_import.csv', index=False)
-    f = open('costs_before_geno_import.csv', 'a')
-    f.write('# units: length in km, total_costs in kEUR\n')
-    costs_before_geno_import.to_csv(f)
-    f.close()
     pd.DataFrame(faulty_grids).to_csv('faulty_grids.csv', index=False)
-    f = open('costs.csv', 'a')
-    f.write('# units: length in km, total_costs in kEUR\n')
-    costs.to_csv(f)
-    f.close()
