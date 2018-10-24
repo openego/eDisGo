3e8aa9f123822d0d3716cdcd13eac2e8eb68dd92
birgits <birgit.schachler@rl-institut.de>
2018-10-24 08:06:33 +0200

    Update version

--- doc/conf.py
+++ doc/conf.py
@@ -106,7 +106,7 @@ author = u'open_eGo-Team'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.6'
+version = '0.0.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class InstallSetup(install):
 
 setup(
     name='eDisGo',
-    version='0.0.7dev',
+    version='0.0.7',
     packages=find_packages(),
     url='https://github.com/openego/eDisGo',
     license='GNU Affero General Public License v3.0',
