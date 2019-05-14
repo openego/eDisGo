6654861979a656570f0af43be2e52e6d535d1b51
birgits <birgit.schachler@rl-institut.de>
2019-05-14 09:17:53 +0200

    Consider quantity in first creation of pypsa representation
    This will not change the results as of now, as ding0 grids are the only
    supported source for grid topologies and line quantity is so far always
    1.

--- edisgo/tools/pypsa_io.py
+++ edisgo/tools/pypsa_io.py
@@ -556,10 +556,13 @@ def lv_to_pypsa(network):
 
         line['type'].append("")
         line['x'].append(
-            l['line'].type['L'] * omega / 1e3 * l['line'].length)
-        line['r'].append(l['line'].type['R'] * l['line'].length)
+            l['line'].type['L'] / l['line'].quantity * omega / 1e3 *
+            l['line'].length)
+        line['r'].append(l['line'].type['R'] / l['line'].quantity *
+                         l['line'].length)
         line['s_nom'].append(
-            sqrt(3) * l['line'].type['I_max_th'] * l['line'].type['U_n'] / 1e3)
+            sqrt(3) * l['line'].type['I_max_th'] * l['line'].type['U_n'] *
+            l['line'].quantity / 1e3)
         line['length'].append(l['line'].length)
 
     # create dataframe representing storages
