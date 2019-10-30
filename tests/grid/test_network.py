12966fda3c3fcdb150c4ba70ba8fed88435794ca
AnyaHe <Anya.Heider@rl-institut.de>
2019-10-30 15:25:42 +0100

    reorganise test_network

--- tests/grid/test_network.py
+++ tests/grid/test_network.py
@@ -2,9 +2,11 @@ import os
 import pandas as pd
 from pandas.util.testing import assert_series_equal
 from math import tan, acos
+import numpy as np
 
 from edisgo.grid.network import Network, TimeSeriesControl
 from edisgo.data import import_data
+from edisgo import EDisGo
 
 
 class TestTimeSeriesControl:
@@ -144,3 +146,25 @@ class TestTimeSeriesControl:
 
         # test when p_nom, type, etc. is missing
         # test for only feed-in or load case
+
+    def test_reinforce(self):
+        self.network.timeseries = TimeSeriesControl(
+            network=self.network,
+            mode='worst-case').timeseries
+        # ToDo: Remove and convert into csv table
+        omega = 2 * np.pi * 50
+        valid_lines = self.network.equipment_data['mv_lines'][
+            self.network.equipment_data['mv_lines'].U_n ==
+            self.network.buses_df.v_nom.iloc[0]]
+        std_line = valid_lines.loc[valid_lines.I_max_th.idxmin()]
+        self.network.lines_df[np.isnan(self.network.lines_df.x)] = self.network.lines_df[
+            np.isnan(self.network.lines_df.x)].assign(num_parallel=1,
+                  r=lambda _: _.length*std_line.loc['R_per_km'],
+                  x=lambda _: _.length*std_line.loc['L_per_km']*omega/1e3,
+                  s_nom=np.sqrt(3)*std_line.loc['I_max_th']*std_line.loc['U_n']/1e3,
+                  type_info=std_line.name)
+
+        timesteps = pd.date_range('1/1/1970', periods=1, freq='H')
+        path = 'C:/Users/Anya.Heider/open_BEA/eDisGo/tests/test_network'
+        edisgo = EDisGo(ding0_grid=path, worst_case_analysis='worst-case')
+        pypsa_network = edisgo.network.to_pypsa(timesteps=timesteps)
