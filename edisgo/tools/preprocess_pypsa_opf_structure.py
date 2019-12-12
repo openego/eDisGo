490b3089f23700216c48074aaef696b5b949b299
RL-INSTITUT\jaap.pedersen <jaappedersen@posteo.net>
2019-12-12 16:16:03 +0100

    new method for preproccesing of PyPsa Network before converting to PM structure used in OPF

---
+++ edisgo/tools/preprocess_pypsa_opf_structure.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+from edisgo.flex_opt.costs import line_expansion_costs
+
+
+def preprocess_pypsa_opf_structure(edisgo_grid, psa_network,mode="mv"):
+
+    mv_grid = edisgo_grid.topology.mv_grid
+
+    # add line expansion costs for all lines
+    line_names = psa_network.lines.index
+    linecosts_df = line_expansion_costs(edisgo_grid, line_names)
+    psa_network.lines = psa_network.lines.join(linecosts_df)
+
+    # check if generator slack has a fluctuating variable set
+    gen_slack_loc = psa_network.generators.control == "Slack"
+
+    is_fluct = psa_network.generators.fluctuating.loc[gen_slack_loc][0]
+    if is_fluct != is_fluct:
+        print("value of fluctuating for slack generator is {}".format(is_fluct))
+        psa_network.generators.fluctuating.loc[gen_slack_loc] = 0
+
+    # add HVMV Trafo to pypsa network and move slack from MV to HV side
+
+    # get dataframe containing hvmv trafos
+    hvmv_trafos = mv_grid.transformers_df
+    # get name of old slack bus
+    if any(psa_network.buses.control == "Slack"):
+        slack_bus_mv = psa_network.buses.loc[psa_network.buses.control == "Slack"]
+    else:
+        slack_bus_mv = psa_network.buses.loc[psa_network.generators.loc[gen_slack_loc].bus[0]]
+
+    # get trafo type from pypsa
+    trafo_type = hvmv_trafos.type.iloc[0]
+    trafo = psa_network.transformer_types.T[trafo_type]
+
+    slack_bus_hv_name = hvmv_trafos.iloc[0].bus0
+
+    slack_bus_hv = pd.DataFrame(slack_bus_mv.copy()).transpose()
+    slack_bus_hv.index = [slack_bus_hv_name]
+    slack_bus_hv.v_nom = trafo["v_nom_0"]
+    slack_bus_hv.control = "Slack"
+
+    buses_df = slack_bus_hv.append(psa_network.buses)
+    psa_network.buses = buses_df
+
+    # Move Generator_slack to new slack bus
+    psa_network.generators.bus.loc[gen_slack_loc] = slack_bus_hv_name
+
+    # Add Transformer to network
+    psa_network.add("Transformer", "Transformer_hvmv_{}".format(psa_network.name), type=trafo_type,
+                    bus0=slack_bus_hv_name, bus1=slack_bus_mv.name)
+
+    t = psa_network.transformers.iloc[0]
+
+    # from pypsa apply_transformer_types(network)
+    # https://github.com/PyPSA/PyPSA/blob/cd21756de30231408d42c1215df1d42254a0b714/pypsa/pf.py#L511
+    # assume simple line model
+    t["r"] = trafo["vscr"]/100.
+    t["x"] = np.sqrt((trafo["vsc"]/100.)**2 - t["r"]**2)
+    t["s_nom"] = sum(mv_grid.transformers_df.s_nom)
+    for attr in ["r","x"]:
+        t[attr] /= len(mv_grid.transformers_df)
+    t["trafo_costs"] = edisgo_grid.config["costs_transformers"][mode]
+
+    psa_network.transformers.iloc[0] = t
+
+    # add new slack bus to dict buses_t
+    for key, val in psa_network.buses_t.items():
+        if len(val.columns) != 0:
+            try:
+                val.insert(0, slack_bus_hv_name, [1.0] * len(psa_network.snapshots))
+            except ValueError as e:
+                print("ValueError: {}".format(e))
+        # print(len(val.columns))
+
+    return
+
