a3eb129c4ae4c812f122cea50951139d2056228d
RL-INSTITUT\jaap.pedersen <jaappedersen@posteo.net>
2020-01-16 14:30:29 +0100

    return figure and ax in opf solution plot

--- edisgo/opf/util/plot_solutions.py
+++ edisgo/opf/util/plot_solutions.py
@@ -9,7 +9,7 @@ def plot_line_expansion(edisgo_obj,timesteps):
     pypsa_plot.buses.x.loc[bus_index] = edisgo_obj.topology.mv_grid.buses_df.x.loc[bus_index]
     pypsa_plot.buses.y.loc[bus_index] = edisgo_obj.topology.mv_grid.buses_df.y.loc[bus_index]
     line_colors = edisgo_obj.opf_results.lines.squeeze()
-    plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 8))
     ax = plt.gca()
     v = np.linspace(min(line_colors), max(line_colors), 101)
     psa_plot = pypsa_plot.plot(line_colors=line_colors, line_cmap=plt.cm.cool, ax=ax, geomap=False)
@@ -17,4 +17,4 @@ def plot_line_expansion(edisgo_obj,timesteps):
     cb.set_clim(vmin=v[0], vmax=v[-1])
     cb.set_label('Number of installed lines')
 
-    return
+    return fig,ax
