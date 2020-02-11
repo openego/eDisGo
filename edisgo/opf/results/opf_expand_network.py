7ad2daf4b4672061eb7bee29ff0029b49eb8e7be
birgits <birgit.schachler@rl-institut.de>
2020-02-11 18:33:08 +0100

    Add function to integrate storage units from OPF into edisgo topology

--- edisgo/opf/results/opf_expand_network.py
+++ edisgo/opf/results/opf_expand_network.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+from edisgo.network.timeseries import add_generators_timeseries, \
+    add_storage_units_timeseries
+import logging
+
+logger = logging.getLogger('edisgo')
 
 
 def expand_network(edisgo, tolerance=1e-6):
@@ -78,3 +83,93 @@ def grid_expansion_costs(opf_results, tolerance=1e-6):
         index=extended_lines)
 
     return costs_df
+
+
+def integrate_storage_units(edisgo, min_storage_size=0.3, timeseries=True,
+                            as_generator=False):
+    """
+    Integrates storage units from OPF into edisgo grid topology.
+
+    Storage units that are too small to be connected to the MV grid or that
+    are not used (time series contains only zeros) are discarded.
+
+    Parameters
+    ----------
+    edisgo : EDisGo object
+    min_storage_size : float
+        Minimal storage size in MW needed to connect storage unit to MV grid.
+        Smaller storage units are ignored.
+    timeseries : bool
+        If True time series is added to component.
+    as_generator : bool
+        If True, storage is added as generator to the edisgo topology. This
+        is temporarily needed as the OPF cannot handle storage units from
+        edisgo yet. This way, storage units with fixed position and time
+        series can be considered in OPF.
+
+    Returns
+    -------
+    list(str), float
+        First return value contains the names of the added storage units and
+        the second return value the capacity of storage units that were too
+        small to connect to the MV grid or not used.
+
+    """
+    storage_cap_discarded = 0
+    added_storage_units = []
+
+    if timeseries:
+        storage_ts = edisgo.opf_results.storage_units_t.ud - \
+                     edisgo.opf_results.storage_units_t.uc
+        reactive_power_ts = pd.DataFrame(0,
+                                         columns=storage_ts.columns,
+                                         index=storage_ts.index)
+
+    # ToDo adding timeseries will only work if timeseries.mode is None
+    # ToDo @Anya why is for mode manual kwarg called 'storage_units_reactive_power'
+    # and for mode None kwarg called 'timeseries_storage_units'
+    for st in edisgo.opf_results.storage_units.index:
+        storage_cap = edisgo.opf_results.storage_units.at[st, 'emax']
+        if storage_cap >= min_storage_size and \
+                (storage_ts.loc[:, st] > 0.001).any():
+            if not as_generator:
+                storage = edisgo.topology.add_storage_unit(
+                    bus=st,
+                    p_nom=storage_cap)  # as C-rate is currently always 1
+            else:
+                storage = edisgo.topology.add_generator(
+                    generator_id=1,
+                    bus=st,
+                    p_nom=storage_cap,
+                    generator_type='storage')
+            if timeseries:
+                ts_active = storage_ts.loc[:, [st]].rename(
+                    columns={st: storage})
+                ts_reactive = reactive_power_ts.loc[:, [st]].rename(
+                    columns={st: storage})
+                if not as_generator:
+                    add_storage_units_timeseries(
+                        edisgo_obj=edisgo,
+                        storage_unit_names=storage,
+                        timeseries_storage_units=ts_active,
+                        timeseries_storage_units_reactive_power=ts_reactive)
+                else:
+                    #ToDo change once fixed in timeseries
+                    edisgo.timeseries.generators_active_power = \
+                        pd.concat(
+                            [edisgo.timeseries.generators_active_power,
+                             ts_active], axis=1)
+                    edisgo.timeseries.generators_reactive_power = \
+                        pd.concat(
+                            [edisgo.timeseries.generators_reactive_power,
+                             ts_reactive], axis=1)
+
+            added_storage_units.append(storage)
+        else:
+            logger.info(
+                "Storage size of storage unit at bus {} is too small and "
+                "therefore discarded.".format(st))
+            storage_cap_discarded += storage_cap
+    return added_storage_units, storage_cap_discarded
+
+
