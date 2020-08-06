f254c8cc523a4ed3adbe0cc02701dd23975b7243
birgits <birgit.schachler@rl-institut.de>
2020-08-06 20:10:55 +0200

    Update example

--- examples/example.py
+++ examples/example.py
@@ -5,18 +5,18 @@ This example shows the general usage of eDisGo. Grid expansion costs for
 distribution grids generated with ding0 are calculated assuming renewable
 and conventional power plant capacities as stated in the scenario framework of
 the German Grid Development Plan 2015 (Netzentwicklungsplan) for the year 2035
-(scenario B2). To determine topology expansion needs worst-case scenarios (heavy
-load flow and reverse power flow) used in conventional topology expansion planning
-are set up.
+(scenario B2). To determine topology expansion needs worst-case scenarios
+(heavy load flow and reverse power flow) used in conventional grid expansion
+planning are set up.
 
 The example assumes you have ding0 grids in the current working directory. If
 you need more information on how to get ding0 grids see the ding0 documentation
 or the Quickstart section of the eDisGo documentation.
 
 As the grids generated with ding0 should represent current stable grids but
-have in some cases stability issues, topology expansion is first conducted before
-connecting future generators in order to obtain stable grids. Final topology
-expansion costs in the DataFrame 'costs' only contain topology expansion costs
+have in some cases stability issues, topology expansion is first conducted
+before connecting future generators in order to obtain stable grids. Final
+grid expansion costs in the DataFrame 'costs' only contain grid expansion costs
 of measures conducted after future generators are connected to the stable
 grids.
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     dingo_grid_path = os.path.join(os.path.dirname(__file__), '460')
 
     # set scenario to define future power plant capacities
-    scenario = None  # 'nep2035'
+    scenario = 'nep2035'
 
     # initialize containers that will hold any error messages
     faulty_grids_before_geno_import = {'topology': [], 'msg': []}
@@ -57,6 +57,8 @@ if __name__ == '__main__':
     # initial topology reinforcement (status quo)
     edisgo.config[
         'grid_expansion_allowed_voltage_deviations'] = {
+        'feedin_case_lower': 0.9,
+        'load_case_upper': 1.1,
         'hv_mv_trafo_offset': 0.04,
         'hv_mv_trafo_control_deviation': 0.0,
         'mv_load_case_max_v_deviation': 0.055,
@@ -82,7 +84,7 @@ if __name__ == '__main__':
     before_geno_import = False
 
     # Import generators
-    #edisgo.import_generators(generator_scenario=scenario)
+    edisgo.import_generators(generator_scenario=scenario)
 
     # Do topology reinforcement
     edisgo.reinforce()
