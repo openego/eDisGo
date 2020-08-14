263643efaf9fba17086b2cecc64cfc75ce827b28
Birgit Schachler <birgit.schachler@rl-institut.de>
2020-08-14 16:32:47 +0200

    Remove old function

--- edisgo/tools/tools.py
+++ edisgo/tools/tools.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import networkx as nx
-from networkx import OrderedGraph
+
 from math import pi, sqrt
 
 from edisgo.flex_opt import exceptions
@@ -156,35 +156,6 @@ def calculate_apparent_power(nominal_voltage, current):
 
     """
     return sqrt(3) * nominal_voltage * current
-
-
-def translate_df_to_graph(buses_df, lines_df, transformers_df=None):
-    graph = OrderedGraph()
-
-    buses = buses_df.index
-    # add nodes
-    graph.add_nodes_from(buses)
-    # add branches
-    branches = []
-    for line_name, line in lines_df.iterrows():
-        branches.append(
-            (
-                line.bus0,
-                line.bus1,
-                {"branch_name": line_name, "length": line.length},
-            )
-        )
-    if transformers_df is not None:
-        for trafo_name, trafo in transformers_df.iterrows():
-            branches.append(
-                (
-                    trafo.bus0,
-                    trafo.bus1,
-                    {"branch_name": trafo_name, "length": 0},
-                )
-            )
-    graph.add_edges_from(branches)
-    return graph
 
 
 def check_bus_for_removal(topology, bus_name):
