7f58a51d978989b0ed03b365e6b82c069ba65376
Birgit Schachler <birgit.schachler@rl-institut.de>
2020-09-16 00:05:54 +0200

    Add real ding0 grid for testing generator import from oedb to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,7 @@ def pytest_configure():
     pytest.ding0_test_network_path = os.path.join(
         os.path.realpath(os.path.dirname(__file__)),
         "ding0_test_network_1")
+
+    pytest.ding0_test_network_2_path = os.path.join(
+        os.path.realpath(os.path.dirname(__file__)),
+        "ding0_test_network_2")
