b66fb48706e6b3827f561586409c1e0aa609b918
birgits <birgit.schachler@rl-institut.de>
2021-07-16 10:49:29 +0200

    Update example and add automatic download of grid data

--- examples/example.py
+++ examples/example.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """
-This example shows the general usage of eDisGo. Grid expansion costs for
-distribution grids generated with ding0 are calculated assuming renewable
-and conventional power plant capacities as stated in the scenario framework of
-the German Grid Development Plan 2015 (Netzentwicklungsplan) for the year 2035
-(scenario B2). To determine topology expansion needs worst-case scenarios
+This example shows how to determine grid expansion costs with eDisGo.
+Grid expansion costs for an example distribution grid generated with ding0
+are calculated assuming renewable and conventional power plant capacities as
+stated in the scenario framework of the German Grid Development Plan 2015
+(Netzentwicklungsplan) for the year 2035 (scenario B2).
+
+To determine topology expansion needs worst-case scenarios
 (heavy load flow and reverse power flow) used in conventional grid expansion
 planning are set up.
 
-The example assumes you have ding0 grids in the current working directory. If
-you need more information on how to get ding0 grids see the ding0 documentation
-or the Quickstart section of the eDisGo documentation.
+The example automatically downloads an example ding0 grid to the current
+working directory in case it does not yet exist. If you want more information
+on how to get more ding0 grids, see the ding0 documentation or the Quickstart
+section of the eDisGo documentation.
 
 As the grids generated with ding0 should represent current stable grids but
 have in some cases stability issues, topology expansion is first conducted
@@ -24,6 +27,7 @@ grids.
 
 import os
 import pandas as pd
+import requests
 
 from edisgo import EDisGo
 from edisgo.network.results import Results
@@ -35,26 +39,37 @@ logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
 
-    # specify path to directory containing ding0 grid csv files
-    dingo_grid_path = os.path.join(os.path.dirname(__file__), '460')
+    # Specify path to directory containing ding0 grid csv files
+    dingo_grid_path = os.path.join(os.path.dirname(__file__),
+                                   'ding0_example_grid')
+    # Download example grid data in case it does not yet exist
+    if not os.path.isdir(dingo_grid_path):
+        logger.debug("Download example grid data.")
+        os.makedirs(dingo_grid_path)
+        file_list = ["buses.csv", "lines.csv",
+                     "generators.csv", "loads.csv",
+                     "transformers.csv", "transformers_hvmv.csv",
+                     "switches.csv", "network.csv"]
+        base_path = "https://raw.githubusercontent.com/openego/eDisGo/" \
+                    "features/refactoring/tests/ding0_test_network_2/"
+        for f in file_list:
+            file = os.path.join(dingo_grid_path, f)
+            req = requests.get(base_path + f)
+            with open(file, "wb") as fout:
+                fout.write(req.content)
 
-    # set scenario to define future power plant capacities
+    # Set scenario to define future power plant capacities
     scenario = 'nep2035'
 
-    # initialize containers that will hold any error messages
-    faulty_grids_before_geno_import = {'topology': [], 'msg': []}
-    faulty_grids = {'topology': [], 'msg': []}
-
-    # set up worst-case scenario
+    # Set up worst-case scenario
     edisgo = EDisGo(ding0_grid=dingo_grid_path,
                     worst_case_analysis='worst-case')
 
-    # Calculate topology expansion costs before generator import
-    logging.info('Grid expansion before generator import.')
-    before_geno_import = True
-
-    # overwrite config parameters for allowed voltage deviations in
-    # initial topology reinforcement (status quo)
+    # Reinforce ding0 grid to obtain a stable status quo grid
+    logging.info('Conduct grid reinforcement to obtain stable '
+                 'status quo grid.')
+    # Overwrite config parameters for allowed voltage deviations in
+    # initial topology reinforcement to better represent currently used limits
     edisgo.config[
         'grid_expansion_allowed_voltage_deviations'] = {
         'feedin_case_lower': 0.9,
@@ -68,33 +83,29 @@ if __name__ == '__main__':
         'mv_lv_station_load_case_max_v_deviation': 0.02,
         'mv_lv_station_feedin_case_max_v_deviation': 0.01
     }
-    # Do topology reinforcement
+    # Conduct reinforcement
     edisgo.reinforce()
-    # Save results
-    edisgo.results.save(
-        'results_grid_{}_before_generator_import'.format(
-            edisgo.topology.id))
 
     # Clear results and reset configs
     edisgo.results = Results(edisgo)
     edisgo.config = None
 
-    # Calculate topology expansion costs after generator import
-    logging.info('Grid expansion after generator import.')
-    before_geno_import = False
+    # Calculate expansion costs for NEP scenario
+    logging.info('Determine grid expansion costs for NEP scenario.')
 
-    # Import generators
+    # Get data on generators in NEP scenario and connect generators to the grid
     edisgo.import_generators(generator_scenario=scenario)
 
-    # Do topology reinforcement
+    # Conduct topology reinforcement
     edisgo.reinforce()
-    # Save results
-    edisgo.results.save('results_grid_{}'.format(
-        edisgo.topology.id))
+
+    logging.info('Grid expansion costs for NEP scenario are: {} kEUR.'.format(
+        edisgo.results.grid_expansion_costs.total_costs.sum()))
+
+    # Save grid expansion results
+    edisgo.results.to_csv(
+        directory=dingo_grid_path,
+        parameters={'grid_expansion_results': None}
+    )
 
     logging.info('SUCCESS!')
-
-    # write error messages to csv files
-    pd.DataFrame(faulty_grids_before_geno_import).to_csv(
-        'faulty_grids_before_geno_import.csv', index=False)
-    pd.DataFrame(faulty_grids).to_csv('faulty_grids.csv', index=False)
