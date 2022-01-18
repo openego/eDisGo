d3da267214a48d7bced3d0afd9b139a7ebf480ea
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-01-18 09:40:47 +0100

    added geopandas and pre-commit hooks packages to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""Setup"""
 import os
 import sys
 
@@ -15,16 +16,37 @@ if sys.version_info[:2] < (3, 7):
 
 
 def read(fname):
+    """
+    Read a text file
+
+    Parameters
+    ----------
+    fname : str or PurePath
+        Path to file
+
+    Returns
+    -------
+    str
+        File content
+
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 class InstallSetup(install):
+    """
+    Run setup installation
+    """
+
     def run(self):
         self.create_edisgo_path()
         install.run(self)
 
     @staticmethod
     def create_edisgo_path():
+        """
+        Create edisgo path if missing
+        """
         edisgo_path = os.path.join(os.path.expanduser("~"), BASEPATH)
         data_path = os.path.join(edisgo_path, "data")
 
@@ -33,6 +55,51 @@ class InstallSetup(install):
         if not os.path.isdir(data_path):
             os.mkdir(data_path)
 
+
+requirements = [
+    "demandlib",
+    "networkx >= 2.5.0",
+    "geopy >= 2.0.0",
+    "pandas >= 1.2.0, < 1.3.0",
+    "geopandas >= 0.9.0",
+    "pyproj >= 3.0.0",
+    "shapely >= 1.7.0",
+    "pypsa >= 0.17.0",
+    "pyomo >= 6.0",
+    "multiprocess",
+    "workalendar",
+    "sqlalchemy < 1.4.0",
+    "geoalchemy2 < 0.7.0",
+    "egoio >= 0.4.7",
+    "matplotlib >= 3.3.0",
+    "pypower",
+    "sklearn",
+    "pydot",
+    "Rtree",
+]
+
+geo_plot_requirements = ["contextily", "descartes"]
+examples_requirements = ["jupyter"]
+dev_requirements = [
+    "pytest",
+    "sphinx_rtd_theme",
+    "pre-commit",
+    "black",
+    "isort",
+    "pyupgrade",
+    "flake8",
+    "pylint",
+]
+full_requirements = list(
+    set(geo_plot_requirements + examples_requirements + dev_requirements)
+)
+
+extras = {
+    "geoplot": geo_plot_requirements,
+    "examples": examples_requirements,
+    "dev": dev_requirements,
+    "full": full_requirements,
+}
 
 setup(
     name="eDisGo",
@@ -45,39 +112,8 @@ setup(
     description="A python package for distribution network analysis and optimization",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    install_requires=[
-        "demandlib",
-        "networkx >= 2.5.0",
-        "geopy >= 2.0.0",
-        "pandas >= 1.2.0, < 1.3.0",
-        "pyproj >= 3.0.0",
-        "shapely >= 1.7.0",
-        "pypsa >= 0.17.0",
-        "pyomo >= 6.0",
-        "multiprocess",
-        "workalendar",
-        "sqlalchemy < 1.4.0",
-        "geoalchemy2 < 0.7.0",
-        "egoio >= 0.4.7",
-        "matplotlib >= 3.3.0",
-        "pypower",
-        "sklearn",
-        "pydot",
-        "Rtree",
-    ],
-    extras_require={
-        "geoplot": ["geopandas >= 0.9.0", "contextily", "descartes"],
-        "examples": ["jupyter"],
-        "dev": ["pytest", "sphinx_rtd_theme"],
-        "full": [
-            "geopandas >= 0.9.0",
-            "contextily",
-            "descartes",
-            "jupyter",
-            "pytest",
-            "sphinx_rtd_theme",
-        ],
-    },
+    install_requires=requirements,
+    extras_require=extras,
     package_data={
         "edisgo": [
             os.path.join("config", "config_system"),
