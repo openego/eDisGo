4533846d5e591ff55f495f6680caa25ac9c2226c
birgits <birgit.schachler@rl-institut.de>
2022-07-29 14:23:27 +0200

    Update docstrings

--- edisgo/io/heat_pump_import.py
+++ edisgo/io/heat_pump_import.py
@@ -9,53 +9,21 @@ logger = logging.getLogger(__name__)
 
 def oedb(edisgo_object, scenario, **kwargs):
     """
-    Gets generator park for specified scenario from oedb and integrates them
-    into the grid.
+    Gets heat pumps for specified scenario from oedb and integrates them into the grid.
 
-    The importer uses SQLAlchemy ORM objects.
-    These are defined in
-    `ego.io <https://github.com/openego/ego.io/tree/dev/egoio/db_tables/>`_.
-
-    For further information see also :attr:`~.EDisGo.import_generators`.
+    See :attr:`~.edisgo.EDisGo.import_heat_pumps` for more information.
 
     Parameters
     ----------
     edisgo_object : :class:`~.EDisGo`
-    generator_scenario : str
-        Scenario for which to retrieve generator data. Possible options
-        are 'nep2035' and 'ego100'.
+    scenario : str
+        Scenario for which to retrieve heat pump data. Possible options
+        are 'eGon2035' and 'eGon100RE'.
 
     Other Parameters
-    ----------------
-    remove_decommissioned : bool
-        If True, removes generators from network that are not included in
-        the imported dataset (=decommissioned). Default: True.
-    update_existing : bool
-        If True, updates capacity of already existing generators to
-        capacity specified in the imported dataset. Default: True.
-    p_target : dict or None
-        Per default, no target capacity is specified and generators are
-        expanded as specified in the respective scenario. However, you may
-        want to use one of the scenarios but have slightly more or less
-        generation capacity than given in the respective scenario. In that case
-        you can specify the desired target capacity per technology type using
-        this input parameter. The target capacity dictionary must have
-        technology types (e.g. 'wind' or 'solar') as keys and corresponding
-        target capacities in MW as values.
-        If a target capacity is given that is smaller than the total capacity
-        of all generators of that type in the future scenario, only some of
-        the generators in the future scenario generator park are installed,
-        until the target capacity is reached.
-        If the given target capacity is greater than that of all generators
-        of that type in the future scenario, then each generator capacity is
-        scaled up to reach the target capacity. Be careful to not have much
-        greater target capacities as this will lead to unplausible generation
-        capacities being connected to the different voltage levels.
-        Also be aware that only technologies specified in the dictionary are
-        expanded. Other technologies are kept the same.
-        Default: None.
+    -----------------
     allowed_number_of_comp_per_lv_bus : int
-        Specifies, how many generators are at most allowed to be placed at
+        Specifies, how many heat pumps are at most allowed to be placed at
         the same LV bus. Default: 2.
 
     Returns
@@ -130,8 +98,8 @@ def _grid_integration(
     on the corresponding building ID.
 
     Grid connection points of heat pumps for district
-    heating are determined based on their geolocation and finding the nearest grid
-    connection point. See :attr:`~.network.topology.Topology.connect_to_mv` and
+    heating are determined based on their geolocation and installed capacity. See
+    :attr:`~.network.topology.Topology.connect_to_mv` and
     :attr:`~.network.topology.Topology.connect_to_lv` for more information.
 
     Parameters
