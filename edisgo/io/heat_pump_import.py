1843cecdeb4a1612a32ddf0ada00080ed7609290
birgits <birgit.schachler@rl-institut.de>
2022-07-29 14:23:13 +0200

    Return names of integrated heat pumps

--- edisgo/io/heat_pump_import.py
+++ edisgo/io/heat_pump_import.py
@@ -58,6 +58,12 @@ def oedb(edisgo_object, scenario, **kwargs):
         Specifies, how many generators are at most allowed to be placed at
         the same LV bus. Default: 2.
 
+    Returns
+    --------
+    list(str)
+        List with names (as in index of :attr:`~.network.topology.Topology.loads_df`)
+        of integrated heat pumps.
+
     """
 
     def _get_individual_heat_pumps(session):
@@ -105,7 +111,7 @@ def oedb(edisgo_object, scenario, **kwargs):
     #     hp_individual = _get_individual_heat_pumps(session)
     #     hp_large = _get_large_heat_pumps(session)
     #
-    # _grid_integration(
+    # return _grid_integration(
     #     edisgo_object=edisgo_object,
     #     hp_individual=hp_individual,
     #     hp_large=hp_large,
@@ -156,13 +162,21 @@ def _grid_integration(
             * geom : :shapely:`Shapely Point object<points>`
                 Geolocation of generator. For CRS see config_grid.srid.
 
+    Returns
+    --------
+    list(str)
+        List with names (as in index of :attr:`~.network.topology.Topology.loads_df`)
+        of integrated heat pumps.
+
     """
+
     raise NotImplementedError
 
     # loads_df = edisgo_object.topology.loads_df
     #
     # # integrate individual heat pumps
     # # get buses corresponding to building IDs
+    # integrated_heat_pumps = hp_individual.index
     # buses = loads_df[loads_df.building_id.isin(hp_individual.building_id)]
     # hp_df = pd.DataFrame(
     #     {
@@ -172,16 +186,16 @@ def _grid_integration(
     #         "weather_cell_id": hp_individual.weather_cell_id,
     #         "sector": "individual_heating",
     #     },
-    #     index=hp_individual.index,
+    #     index=integrated_heat_pumps,
     # )
     # edisgo_object.topology.loads_df = pd.concat([loads_df, hp_df])
     # logger.debug(
-    #     f"{sum(hp_df.p_set)} MW of heat pumps for individual heating " f"integrated."
+    #     f"{sum(hp_df.p_set)} MW of heat pumps for individual heating integrated."
     # )
     #
     # # integrate large heat pumps
     # for hp in hp_large:
-    #     edisgo_object.integrate_component_based_on_geolocation(
+    #     hp_name = edisgo_object.integrate_component_based_on_geolocation(
     #         comp_type="heat_pump",
     #         geolocation=hp_large.at[hp, "geom"],
     #         add_ts=False,
@@ -189,7 +203,9 @@ def _grid_integration(
     #         weather_cell_id=hp_large.at[hp, "weather_cell_id"],
     #         kwargs={"sector": "district_heating"},
     #     )
+    #     integrated_heat_pumps = integrated_heat_pumps.append(hp_name)
     #
     # logger.debug(
     #     f"{sum(hp_large.p_set)} MW of heat pumps for district heating " f"integrated."
     # )
+    # return integrated_heat_pumps
