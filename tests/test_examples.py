420c2c0e4f9d5331ea313d42385e4d92be1b68b4
mltja <malte.jahn@rl-institut.com>
2022-09-27 10:13:06 +0200

    Fix test examples
    - Change the testing of notebook to pytest-notebook for better windows compatibility
    - Add comment in electromobility_example.ipynb
    - Fix Error in electromobility_example.ipynb through false filepath
      - Get module path instead of non explicit definition of filepath.
      - Testing the notebooks is using tempfiles there the normal paths are pointing to nothing.
    
    ´´´
    from edisgo import __file__
    module_path = os.path.dirname(os.path.dirname(__file__))
    ´´´

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,6 @@
 import logging
 import os
-import subprocess
-import tempfile
 
-import nbformat
 import pytest
 import pytest_notebook
 
@@ -15,64 +12,44 @@ class TestExamples:
             os.path.dirname(os.path.dirname(__file__)), "examples"
         )
 
-    def _notebook_run(self, path):
-        """
-        Execute a notebook via nbconvert and collect output.
-        Returns (parsed nb object, execution errors)
-        """
-        dirname, __ = os.path.split(path)
-        os.chdir(dirname)
-        with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-            args = [
-                "jupyter",
-                "nbconvert",
-                path,
-                "--output",
-                fout.name,
-                "--to",
-                "notebook",
-                "--execute",
-                "--ExecutePreprocessor.timeout=90",
-            ]
-            subprocess.check_call(args)
-
-            fout.seek(0)
-            nb = nbformat.read(fout, nbformat.current_nbformat)
-
-        errors = [
-            output
-            for cell in nb.cells
-            if "outputs" in cell
-            for output in cell["outputs"]
-            if output.output_type == "error"
-        ]
-
-        return nb, errors
-
     @pytest.mark.slow
     def test_plot_example_ipynb(self):
         path = os.path.join(self.examples_dir_path, "plot_example.ipynb")
         notebook = pytest_notebook.notebook.load_notebook(path=path)
         result = pytest_notebook.execution.execute_notebook(
-            notebook, with_coverage=False, timeout=20
+            notebook,
+            with_coverage=False,
         )
+        if result.exec_error is not None:
+            print(result.exec_error)
         assert result.exec_error is None
 
     @pytest.mark.slow
     def test_electromobility_example_ipynb(self):
-        nb, errors = self._notebook_run(
-            os.path.join(self.examples_dir_path, "electromobility_example.ipynb")
+        path = os.path.join(self.examples_dir_path, "electromobility_example.ipynb")
+        notebook = pytest_notebook.notebook.load_notebook(path=path)
+        result = pytest_notebook.execution.execute_notebook(
+            notebook,
+            with_coverage=False,
         )
-        assert errors == []
+        if result.exec_error is not None:
+            print(result.exec_error)
+        assert result.exec_error is None
 
     @pytest.mark.slow
     def test_edisgo_simple_example_ipynb(self):
-        nb, errors = self._notebook_run(
-            os.path.join(self.examples_dir_path, "edisgo_simple_example.ipynb")
+        path = os.path.join(self.examples_dir_path, "edisgo_simple_example.ipynb")
+        notebook = pytest_notebook.notebook.load_notebook(path=path)
+        result = pytest_notebook.execution.execute_notebook(
+            notebook,
+            with_coverage=False,
         )
-        assert errors == []
+        if result.exec_error is not None:
+            print(result.exec_error)
+        assert result.exec_error is None
 
     @classmethod
     def teardown_class(cls):
         logger = logging.getLogger("edisgo")
+        logger.handlers.clear()
         logger.propagate = True
