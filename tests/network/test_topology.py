67792a6bc725849cf720ecc3a30ddf6a0fa19dd8
birgits <birgit.schachler@rl-institut.de>
2023-01-13 10:54:17 +0100

    Fix test - number of lines changed as 1 m end lines are not deleted automatically anymore

--- tests/network/test_topology.py
+++ tests/network/test_topology.py
@@ -856,7 +856,7 @@ class TestTopology:
         # check number of lines where buses were exchanged
         assert (lines_df_before.bus0 == self.topology.lines_df.bus0).value_counts().loc[
             False
-        ] == 9
+        ] == 11
 
     def test_to_csv(self):
         """Test for method to_csv."""
