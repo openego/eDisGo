362cf7b21a77c12c8a84127ef2499289c537dc1f
birgits <birgit.schachler@rl-institut.de>
2023-01-30 13:45:27 +0100

    Add functions reduce_memory, to_csv and from_csv to OverlyingGrid class

--- edisgo/network/overlying_grid.py
+++ edisgo/network/overlying_grid.py
@@ -1,3 +1,7 @@
+import os
+
+from zipfile import ZipFile
+
 import pandas as pd
 
 
@@ -70,3 +74,135 @@ class OverlyingGrid:
             "geothermal_energy_feedin_district_heating",
             "solarthermal_energy_feedin_district_heating",
         ]
+
+    def reduce_memory(self, attr_to_reduce=None, to_type="float32"):
+        """
+        Reduces size of time series data to save memory.
+
+        Parameters
+        -----------
+        attr_to_reduce : list(str), optional
+            List of attributes to reduce size for. Per default, all time series data
+            are reduced.
+        to_type : str, optional
+            Data type to convert time series data to. This is a tradeoff
+            between precision and memory. Default: "float32".
+
+        """
+        if attr_to_reduce is None:
+            attr_to_reduce = self._attributes
+        for attr in attr_to_reduce:
+            if isinstance(getattr(self, attr), pd.Series):
+                setattr(
+                    self,
+                    attr,
+                    getattr(self, attr).astype(to_type),
+                )
+            else:
+                setattr(
+                    self,
+                    attr,
+                    getattr(self, attr).apply(lambda _: _.astype(to_type)),
+                )
+
+    def to_csv(self, directory, reduce_memory=False, **kwargs):
+        """
+        Saves data in object to csv.
+
+        Parameters
+        ----------
+        directory : str
+            Directory to save data in.
+        reduce_memory : bool, optional
+            If True, size of time series data is reduced using
+            :attr:`~.network.overlying_grid.OverlyingGrid.reduce_memory`.
+            Optional parameters of
+            :attr:`~.network.overlying_grid.OverlyingGrid.reduce_memory`
+            can be passed as kwargs to this function. Default: False.
+
+        Other Parameters
+        ------------------
+        kwargs :
+            Kwargs may contain arguments of
+            :attr:`~.network.overlying_grid.OverlyingGrid.reduce_memory`.
+
+        """
+        if reduce_memory is True:
+            self.reduce_memory(**kwargs)
+
+        os.makedirs(directory, exist_ok=True)
+
+        for attr in self._attributes:
+            if not getattr(self, attr).empty:
+                if isinstance(getattr(self, attr), pd.Series):
+                    getattr(self, attr).to_frame(name=attr).to_csv(
+                        os.path.join(directory, f"{attr}.csv")
+                    )
+                else:
+                    getattr(self, attr).to_csv(os.path.join(directory, f"{attr}.csv"))
+
+    def from_csv(
+        self,
+        data_path,
+        dtype=None,
+        from_zip_archive=False,
+        **kwargs,
+    ):
+        """
+        Restores data in object from csv files.
+
+        Parameters
+        ----------
+        data_path : str
+            Path to directory to obtain data from. Must be a directory or zip
+            archive.
+        dtype : str, optional
+            Numerical data type for data to be loaded from csv. E.g. "float32".
+            Default: None.
+        from_zip_archive : bool, optional
+            Set True if data is archived in a zip archive. Default: False.
+
+        """
+
+        # get all attributes
+        attrs = self._attributes
+
+        if from_zip_archive:
+            # read from zip archive
+            # setup ZipFile Class
+            zip = ZipFile(data_path)
+
+            # get all directories and files within zip archive
+            files = zip.namelist()
+
+            # add directory and .csv to files to match zip archive
+            attrs = {v: f"overlying_grid/{v}.csv" for v in attrs}
+
+        else:
+            # read from directory
+            # check files within the directory
+            files = os.listdir(data_path)
+
+            # add .csv to files to match directory structure
+            attrs = {v: f"{v}.csv" for v in attrs}
+
+        attrs_to_read = {k: v for k, v in attrs.items() if v in files}
+
+        for attr, file in attrs_to_read.items():
+            if from_zip_archive:
+                # open zip file to make it readable for pandas
+                with zip.open(file) as f:
+                    df = pd.read_csv(f, index_col=0, parse_dates=True, dtype=dtype)
+            else:
+                path = os.path.join(data_path, file)
+                df = pd.read_csv(path, index_col=0, parse_dates=True, dtype=dtype)
+
+            # convert to Series if Series is expected
+            if isinstance(getattr(self, attr), pd.Series):
+                df = df.squeeze("columns")
+
+            setattr(self, attr, df)
+
+        if from_zip_archive:
+            # make sure to destroy ZipFile Class to close any open connections
+            zip.close()
