4e63e5d14ed0a52c0f56f61e58414428fc96859a
Julian.Endres <julian.endres@rl-institut.de>
2023-02-06 09:50:47 +0100

    Revert "Add 'opt' column to loads_df definition"
    This reverts commit 8f0d6c60221164cc737b2a6605cf24226877e624.

--- edisgo/network/topology.py
+++ edisgo/network/topology.py
@@ -31,7 +31,7 @@ if "READTHEDOCS" not in os.environ:
 logger = logging.getLogger(__name__)
 
 COLUMNS = {
-    "loads_df": ["bus", "p_set", "type", "annual_consumption", "sector", "opt"],
+    "loads_df": ["bus", "p_set", "type", "annual_consumption", "sector"],
     "generators_df": [
         "bus",
         "p_nom",
