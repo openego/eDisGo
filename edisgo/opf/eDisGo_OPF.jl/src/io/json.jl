cf75c759d5782b2c9b4bda1c12133aabc75104af
Maike Held <Maike.Held@rl-institut.de>
2023-02-07 07:15:49 +0100

    Minor fix

--- edisgo/opf/eDisGo_OPF.jl/src/io/json.jl
+++ edisgo/opf/eDisGo_OPF.jl/src/io/json.jl
@@ -2,7 +2,7 @@
 function parse_json(io::Union{IO,String}; kwargs...)::Dict{String,Any}
     pm_data = JSON.parse(io)
 
-    _jsonver2juliaver!(pm_data)
+    PowerModels._jsonver2juliaver!(pm_data)
 
     if haskey(pm_data, "conductors")
         Memento.warn(_LOGGER, "The JSON data contains the conductor parameter, but only single conductors are supported.  Consider using PowerModelsDistribution.")
