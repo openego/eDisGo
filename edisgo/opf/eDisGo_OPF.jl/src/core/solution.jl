61a5fe943cda4deb8538ce382df8beaabadac59d
Maike Held <Maike.Held@rl-institut.de>
2023-02-20 12:13:26 +0100

    SOC check only for branches, not virtual (storage) branches

--- edisgo/opf/eDisGo_OPF.jl/src/core/solution.jl
+++ edisgo/opf/eDisGo_OPF.jl/src/core/solution.jl
@@ -9,13 +9,14 @@ end
 function check_SOC_equality(result, data_edisgo)
     timesteps = keys(result["solution"]["nw"])
     branches = keys(data_edisgo["branch"])
-    branch_f_bus = Dict(k => string(data_edisgo["branch"][k]["f_bus"]) for k in branches)
+    branches_wo_storage = [branch for branch in branches if !(data_edisgo["branch"][branch]["storage"])]
+    branch_f_bus = Dict(k => string(data_edisgo["branch"][k]["f_bus"]) for k in branches_wo_storage)
     soc_eq_dict = Dict()
     soc_tight = true
     for t in timesteps
         eq_res = Dict(b => (result["solution"]["nw"][t]["branch"][b]["pf"]^2
         + result["solution"]["nw"][t]["branch"][b]["qf"]^2
-        -result["solution"]["nw"][t]["branch"][b]["ccm"]*result["solution"]["nw"][t]["bus"][branch_f_bus[b]]["w"]) for b in branches)
+        -result["solution"]["nw"][t]["branch"][b]["ccm"]*result["solution"]["nw"][t]["bus"][branch_f_bus[b]]["w"]) for b in branches_wo_storage)
         soc_eq_dict[t]= filter(((k,v),) ->  v <-1e-2, eq_res) # Bobo Second-order cone relaxations of the optimal power flow for active distribution grids: Comparison of methods
         if length(keys(soc_eq_dict[t])) > 0
             soc_tight = false
