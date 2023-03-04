8732adacd3e11fa4ec2c745a4d62786144c6d819
birgits <birgit.schachler@rl-institut.de>
2023-03-04 13:53:22 +0100

    Change test class attributes

--- tests/network/test_heat.py
+++ tests/network/test_heat.py
@@ -10,23 +10,26 @@ from edisgo.network.heat import HeatPump
 
 class TestHeatPump:
     @classmethod
-    def setup_class(self):
-        self.timeindex = pd.date_range("1/1/2011 12:00", periods=2, freq="H")
-        self.cop = pd.DataFrame(
+    def setup_class(cls):
+        # set up data used in tests
+        cls.heatpump = HeatPump()
+
+        cls.timeindex = pd.date_range("1/1/2011 12:00", periods=2, freq="H")
+        cls.cop = pd.DataFrame(
             data={
                 "hp1": [5.0, 6.0],
                 "hp2": [7.0, 8.0],
             },
-            index=self.timeindex,
+            index=cls.timeindex,
         )
-        self.heat_demand = pd.DataFrame(
+        cls.heat_demand = pd.DataFrame(
             data={
                 "hp1": [1.0, 2.0],
                 "hp2": [3.0, 4.0],
             },
-            index=self.timeindex,
+            index=cls.timeindex,
         )
-        self.tes = pd.DataFrame(
+        cls.tes = pd.DataFrame(
             data={
                 "capacity": [9.5, 10.5],
                 "efficiency": [0.9, 0.8],
@@ -34,13 +37,31 @@ class TestHeatPump:
             },
             index=["hp1", "hp2"],
         )
+        cls.hp_data_egon = cls.setup_egon_heat_pump_data(cls)
 
-    @pytest.fixture(autouse=True)
-    def setup_heat_pump(self):
-        self.heatpump = HeatPump()
-        self.heatpump.cop_df = self.cop
-        self.heatpump.heat_demand_df = self.heat_demand
-        self.heatpump.thermal_storage_units_df = self.tes
+    def setup_egon_heat_pump_data(self):
+        names = [
+            "HP_442081",
+            "Heat_Pump_LVGrid_1163850014_district_heating_6",
+            "HP_448156",
+        ]
+        building_ids = [442081, None, 448156]
+        sector = ["individual_heating", "district_heating", "individual_heating"]
+        weather_cell_ids = [11051, 11051, 11052]
+        district_heating_ids = [None, 5, None]
+        hp_df = pd.DataFrame(
+            data={
+                "bus": "dummy_bus",
+                "p_set": 1.0,
+                "building_id": building_ids,
+                "type": "heat_pump",
+                "sector": sector,
+                "weather_cell_id": weather_cell_ids,
+                "district_heating_id": district_heating_ids,
+            },
+            index=names,
+        )
+        return hp_df
 
     def test_set_cop(self):
         self.edisgo = EDisGo(ding0_grid=pytest.ding0_test_network_path)
@@ -78,6 +99,9 @@ class TestHeatPump:
 
     def test_reduce_memory(self):
 
+        self.heatpump.cop_df = self.cop
+        self.heatpump.heat_demand_df = self.heat_demand
+
         # check with default value
         assert (self.heatpump.cop_df.dtypes == "float64").all()
         assert (self.heatpump.heat_demand_df.dtypes == "float64").all()
@@ -98,6 +122,10 @@ class TestHeatPump:
         self.heatpump.reduce_memory()
 
     def test_to_csv(self):
+
+        self.heatpump.cop_df = self.cop
+        self.heatpump.heat_demand_df = self.heat_demand
+        self.heatpump.thermal_storage_units_df = self.tes
 
         # test with default values
         save_dir = os.path.join(os.getcwd(), "heat_pump_csv")
@@ -121,6 +149,10 @@ class TestHeatPump:
         shutil.rmtree(save_dir, ignore_errors=True)
 
     def test_from_csv(self):
+
+        self.heatpump.cop_df = self.cop
+        self.heatpump.heat_demand_df = self.heat_demand
+        self.heatpump.thermal_storage_units_df = self.tes
 
         # write to csv
         save_dir = os.path.join(os.getcwd(), "heat_pump_csv")
