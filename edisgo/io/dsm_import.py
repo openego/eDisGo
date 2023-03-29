53a605d472dbfb8e46ae1dd85199a433edad4b39
birgits <birgit.schachler@rl-institut.de>
2023-03-29 16:40:29 +0200

    Refactor DSM import from database

--- edisgo/io/dsm_import.py
+++ edisgo/io/dsm_import.py
@@ -4,12 +4,15 @@ import logging
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 import pandas as pd
 import saio
 
 from sqlalchemy.engine.base import Engine
 
 from edisgo.io.db import session_scope_egon_data
+from edisgo.io.timeseries_import import _timeindex_helper_func
+from edisgo.tools import tools
 
 if TYPE_CHECKING:
     from edisgo import EDisGo
@@ -17,103 +20,369 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def dsm_from_database(
+def oedb(
     edisgo_obj: EDisGo,
+    scenario: str,
     engine: Engine,
-    scenario: str = "eGon2035",
+    timeindex=None,
 ):
-    saio.register_schema("grid", engine)
+    """
+    Gets industrial and CTS DSM profiles from the
+    `OpenEnergy DataBase <https://openenergy-platform.org/dataedit/schemas>`_.
 
-    from saio.grid import (
-        egon_etrago_link,
-        egon_etrago_link_timeseries,
-        egon_etrago_store,
-        egon_etrago_store_timeseries,
+    Profiles comprise minimum and maximum load increase in MW as well as maximum energy
+    pre- and postponing in MWh.
+
+    Parameters
+    ----------
+    edisgo_object : :class:`~.EDisGo`
+    scenario : str
+        Scenario for which to retrieve DSM data. Possible options
+        are 'eGon2035' and 'eGon100RE'.
+    engine : :sqlalchemy:`sqlalchemy.Engine<sqlalchemy.engine.Engine>`
+        Database engine.
+    timeindex : :pandas:`pandas.DatetimeIndex<DatetimeIndex>` or None
+        Specifies time steps for which to return data. Leap years can currently
+        not be handled. In case the given timeindex contains a leap year, the data will
+        be indexed using the default year (2035 in case of the 'eGon2035' and to 2045
+        in case of the 'eGon100RE' scenario) and returned for the whole year.
+        If no timeindex is provided, the timeindex set in
+        :py:attr:`~.network.timeseries.TimeSeries.timeindex` is used.
+        If :py:attr:`~.network.timeseries.TimeSeries.timeindex` is not set, the data
+        is indexed using the default year and returned for the whole year.
+
+    Returns
+    --------
+    dict(str, :pandas:`pandas.DataFrame<DataFrame>`)
+        Dictionary with DSM data with keys `p_min`, `p_max`, `e_min` and `e_max` (see
+        :class:`~.network.dsm.DSM` for more information). Values contain dataframes with
+        DSM profiles per load for one year in an hourly resolution in MW. Index of the
+        dataframes are time indices. Columns contain the load name the DSM profile is
+        associated with as in index of :attr:`~.network.topology.Topology.loads_df`.
+
+    """
+    # get CTS and industrial DSM profiles
+    dsm_cts = get_profile_cts(edisgo_obj, scenario, engine)
+    ind_loads = edisgo_obj.topology.loads_df[
+        (edisgo_obj.topology.loads_df.type == "conventional_load")
+        & (edisgo_obj.topology.loads_df.sector == "industry")
+    ]
+    dsm_ind = get_profiles_per_industrial_load(
+        ind_loads.building_id.unique(), scenario, engine
     )
 
-    with session_scope_egon_data(engine) as session:
-        query = session.query(egon_etrago_link).filter(
-            egon_etrago_link.scn_name == "eGon2035",
-            egon_etrago_link.carrier == "dsm",
-            egon_etrago_link.bus0 == edisgo_obj.topology.id,
+    # rename industrial DSM profiles, join with CTS profiles and set time index
+    rename_series = (
+        ind_loads.loc[:, ["building_id"]]
+        .dropna()
+        .reset_index()
+        .set_index("building_id")
+        .iloc[:, 0]
+    )
+    timeindex, timeindex_full = _timeindex_helper_func(
+        edisgo_obj,
+        timeindex,
+        default_year=tools.get_year_based_on_scenario(scenario),
+        allow_leap_year=False,
+    )
+    dsm_ind_cts = {}
+    for dsm_profile in ["e_min", "e_max", "p_min", "p_max"]:
+        dsm_ind[dsm_profile].rename(columns=rename_series, inplace=True)
+        dsm_ind_cts_tmp = pd.concat(
+            [dsm_cts[dsm_profile], dsm_ind[dsm_profile]], axis=1
         )
+        dsm_ind_cts_tmp.index = timeindex_full
+        dsm_ind_cts[dsm_profile] = dsm_ind_cts_tmp.loc[timeindex, :]
 
-        edisgo_obj.dsm.egon_etrago_link = pd.read_sql(
-            sql=query.statement, con=query.session.bind
-        )
+    return dsm_ind_cts
 
-    link_id = edisgo_obj.dsm.egon_etrago_link.at[0, "link_id"]
-    store_bus_id = edisgo_obj.dsm.egon_etrago_link.at[0, "bus1"]
-    p_nom = edisgo_obj.dsm.egon_etrago_link.at[0, "p_nom"]
 
-    with session_scope_egon_data(engine) as session:
-        query = session.query(egon_etrago_link_timeseries).filter(
-            egon_etrago_link_timeseries.scn_name == scenario,
-            egon_etrago_link_timeseries.link_id == link_id,
-        )
+def get_profiles_per_industrial_load(
+    load_ids,
+    scenario: str,
+    engine: Engine,
+):
+    """
+    Gets industrial DSM profiles per site and OSM area.
 
-        edisgo_obj.dsm.egon_etrago_link_timeseries = pd.read_sql(
-            sql=query.statement, con=query.session.bind
-        )
+    Parameters
+    ----------
+    load_ids : list(int)
+        List of industrial site and OSM IDs to retrieve DSM profiles for.
+    scenario : str
+        Scenario for which to retrieve DSM data. Possible options
+        are 'eGon2035' and 'eGon100RE'.
+    engine : :sqlalchemy:`sqlalchemy.Engine<sqlalchemy.engine.Engine>`
+        Database engine.
 
-    for p in ["p_min_pu", "p_max_pu"]:
-        name = "_".join(p.split("_")[:-1])
+    Returns
+    --------
+    dict(str, :pandas:`pandas.DataFrame<DataFrame>`)
+        Dictionary with DSM data with keys `p_min`, `p_max`, `e_min` and `e_max`. Values
+        contain dataframes with DSM profiles per site and OSM area for one year in an
+        hourly resolution in MW. Index contains hour of the year (from 0 to 8759) and
+        column names are site ID as integer.
 
-        data = {name: edisgo_obj.dsm.egon_etrago_link_timeseries.at[0, p]}
+    """
+    saio.register_schema("demand", engine)
+    from saio.demand import (
+        egon_demandregio_sites_ind_electricity_dsm_timeseries as sites_ind_dsm_ts,
+    )
+    from saio.demand import (
+        egon_osm_ind_load_curves_individual_dsm_timeseries,
+        egon_sites_ind_load_curves_individual_dsm_timeseries,
+    )
 
-        if len(edisgo_obj.timeseries.timeindex) != len(data[name]):
-            raise IndexError(
-                f"The length of the time series of the edisgo object ("
-                f"{len(edisgo_obj.timeseries.timeindex)}) and the database ("
-                f"{len(data[name])}) do not match. Adjust the length of "
-                f"the time series of the edisgo object accordingly."
+    dsm_dict = {}
+
+    if hasattr(egon_sites_ind_load_curves_individual_dsm_timeseries, "p_min_pu"):
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                egon_sites_ind_load_curves_individual_dsm_timeseries.site_id,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.p_nom,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.p_min_pu,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.p_max_pu,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.e_nom,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.e_min_pu,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.e_max_pu,
+            ).filter(
+                egon_sites_ind_load_curves_individual_dsm_timeseries.scn_name
+                == scenario,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.site_id.in_(
+                    load_ids
+                ),
             )
 
-        setattr(
-            edisgo_obj.dsm,
-            name,
-            pd.DataFrame(data, index=edisgo_obj.timeseries.timeindex).mul(p_nom),
-        )
+            df_sites_1 = pd.read_sql(sql=query.statement, con=engine)
 
-    with session_scope_egon_data(engine) as session:
-        query = session.query(egon_etrago_store).filter(
-            egon_etrago_store.scn_name == scenario,
-            egon_etrago_store.carrier == "dsm",
-            egon_etrago_store.bus == store_bus_id,
-        )
-
-        edisgo_obj.dsm.egon_etrago_store = pd.read_sql(
-            sql=query.statement, con=query.session.bind
-        )
-
-    store_id = edisgo_obj.dsm.egon_etrago_store.at[0, "store_id"]
-    e_nom = edisgo_obj.dsm.egon_etrago_store.at[0, "e_nom"]
-
-    with session_scope_egon_data(engine) as session:
-        query = session.query(egon_etrago_store_timeseries).filter(
-            egon_etrago_store_timeseries.scn_name == scenario,
-            egon_etrago_store_timeseries.store_id == store_id,
-        )
-
-        edisgo_obj.dsm.egon_etrago_store_timeseries = pd.read_sql(
-            sql=query.statement, con=query.session.bind
-        )
-
-    for e in ["e_min_pu", "e_max_pu"]:
-        name = "_".join(e.split("_")[:-1])
-
-        data = {name: edisgo_obj.dsm.egon_etrago_store_timeseries.at[0, e]}
-
-        if len(edisgo_obj.timeseries.timeindex) != len(data[name]):
-            raise IndexError(
-                f"The length of the time series of the edisgo object ("
-                f"{len(edisgo_obj.timeseries.timeindex)}) and the database ("
-                f"{len(data[name])}) do not match. Adjust the length of "
-                f"the time series of the edisgo object accordingly."
+            query = session.query(
+                sites_ind_dsm_ts.industrial_sites_id.label("site_id"),
+                sites_ind_dsm_ts.p_nom,
+                sites_ind_dsm_ts.p_min_pu,
+                sites_ind_dsm_ts.p_max_pu,
+                sites_ind_dsm_ts.e_nom,
+                sites_ind_dsm_ts.e_min_pu,
+                sites_ind_dsm_ts.e_max_pu,
+            ).filter(
+                sites_ind_dsm_ts.scn_name == scenario,
+                sites_ind_dsm_ts.industrial_sites_id.in_(load_ids),
             )
 
-        setattr(
-            edisgo_obj.dsm,
-            name,
-            pd.DataFrame(data, index=edisgo_obj.timeseries.timeindex).mul(e_nom),
-        )
+            df_sites_2 = pd.read_sql(sql=query.statement, con=engine)
+
+            query = session.query(
+                egon_osm_ind_load_curves_individual_dsm_timeseries.osm_id.label(
+                    "site_id"
+                ),
+                egon_osm_ind_load_curves_individual_dsm_timeseries.p_nom,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.p_min_pu,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.p_max_pu,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.e_nom,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.e_min_pu,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.e_max_pu,
+            ).filter(
+                egon_osm_ind_load_curves_individual_dsm_timeseries.scn_name == scenario,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.osm_id.in_(load_ids),
+            )
+
+            df_areas = pd.read_sql(sql=query.statement, con=engine)
+
+        df = pd.concat([df_sites_1, df_sites_2, df_areas])
+        # add time step column
+        df["time_step"] = len(df) * [np.arange(0, 8760)]
+        # un-nest time series data and pivot so that time_step becomes index and
+        # site_id column names
+        dsm_dict["p_min"] = _pivot_pu_helper(df, "p_min", "p_nom")
+        dsm_dict["p_max"] = _pivot_pu_helper(df, "p_max", "p_nom")
+        dsm_dict["e_min"] = _pivot_pu_helper(df, "e_min", "e_nom")
+        dsm_dict["e_max"] = _pivot_pu_helper(df, "e_max", "e_nom")
+
+    else:
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                egon_sites_ind_load_curves_individual_dsm_timeseries.site_id,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.p_min,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.p_max,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.e_min,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.e_max,
+            ).filter(
+                egon_sites_ind_load_curves_individual_dsm_timeseries.scn_name
+                == scenario,
+                egon_sites_ind_load_curves_individual_dsm_timeseries.site_id.in_(
+                    load_ids
+                ),
+            )
+
+            df_sites_1 = pd.read_sql(sql=query.statement, con=engine)
+
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                sites_ind_dsm_ts.industrial_sites_id.label("site_id"),
+                sites_ind_dsm_ts.p_min,
+                sites_ind_dsm_ts.p_max,
+                sites_ind_dsm_ts.e_min,
+                sites_ind_dsm_ts.e_max,
+            ).filter(
+                sites_ind_dsm_ts.scn_name == scenario,
+                sites_ind_dsm_ts.industrial_sites_id.in_(load_ids),
+            )
+
+            df_sites_2 = pd.read_sql(sql=query.statement, con=engine)
+
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                egon_osm_ind_load_curves_individual_dsm_timeseries.osm_id.label(
+                    "site_id"
+                ),
+                egon_osm_ind_load_curves_individual_dsm_timeseries.p_min,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.p_max,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.e_min,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.e_max,
+            ).filter(
+                egon_osm_ind_load_curves_individual_dsm_timeseries.scn_name == scenario,
+                egon_osm_ind_load_curves_individual_dsm_timeseries.osm_id.in_(load_ids),
+            )
+
+            df_areas = pd.read_sql(sql=query.statement, con=engine)
+
+        df = pd.concat([df_sites_1, df_sites_2, df_areas])
+        # add time step column
+        df["time_step"] = len(df) * [np.arange(0, 8760)]
+        # un-nest time series data and pivot so that time_step becomes index and
+        # site_id column names
+        dsm_dict["p_min"] = _pivot_abs_helper(df, "p_min")
+        dsm_dict["p_max"] = _pivot_abs_helper(df, "p_max")
+        dsm_dict["e_min"] = _pivot_abs_helper(df, "e_min")
+        dsm_dict["e_max"] = _pivot_abs_helper(df, "e_max")
+
+    return dsm_dict
+
+
+def get_profile_cts(
+    edisgo_obj: EDisGo,
+    scenario: str,
+    engine: Engine,
+):
+    """
+    Gets CTS DSM profiles for all CTS loads in the MV grid.
+
+    Parameters
+    ----------
+    edisgo_object : :class:`~.EDisGo`
+    scenario : str
+        Scenario for which to retrieve DSM data. Possible options
+        are 'eGon2035' and 'eGon100RE'.
+    engine : :sqlalchemy:`sqlalchemy.Engine<sqlalchemy.engine.Engine>`
+        Database engine.
+
+    Returns
+    --------
+    dict(str, :pandas:`pandas.DataFrame<DataFrame>`)
+        Dictionary with DSM data with keys `p_min`, `p_max`, `e_min` and `e_max`. Values
+        contain dataframes with DSM profiles per CTS load for one year in an
+        hourly resolution in MW. Index contains hour of the year (from 0 to 8759) and
+        column names are site ID as integer.
+
+    """
+    saio.register_schema("demand", engine)
+    from saio.demand import egon_etrago_electricity_cts_dsm_timeseries
+
+    # get data
+    dsm_dict = {}
+    if hasattr(egon_etrago_electricity_cts_dsm_timeseries, "p_min_pu"):
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                egon_etrago_electricity_cts_dsm_timeseries.bus.label("site_id"),
+                egon_etrago_electricity_cts_dsm_timeseries.p_nom,
+                egon_etrago_electricity_cts_dsm_timeseries.p_min_pu,
+                egon_etrago_electricity_cts_dsm_timeseries.p_max_pu,
+                egon_etrago_electricity_cts_dsm_timeseries.e_nom,
+                egon_etrago_electricity_cts_dsm_timeseries.e_min_pu,
+                egon_etrago_electricity_cts_dsm_timeseries.e_max_pu,
+            ).filter(
+                egon_etrago_electricity_cts_dsm_timeseries.scn_name == scenario,
+                egon_etrago_electricity_cts_dsm_timeseries.bus
+                == edisgo_obj.topology.id,
+            )
+            df = pd.read_sql(sql=query.statement, con=engine)
+        # add time step column
+        df["time_step"] = len(df) * [np.arange(0, 8760)]
+        # un-nest time series data and pivot so that time_step becomes index and
+        # site_id column names
+        dsm_dict["p_min"] = _pivot_pu_helper(df, "p_min", "p_nom")
+        dsm_dict["p_max"] = _pivot_pu_helper(df, "p_max", "p_nom")
+        dsm_dict["e_min"] = _pivot_pu_helper(df, "e_min", "e_nom")
+        dsm_dict["e_max"] = _pivot_pu_helper(df, "e_max", "e_nom")
+
+        # temporary! set negative p_max and e_max values and positive p_min and e_min
+        # values to zero
+        dsm_dict["p_min"][dsm_dict["p_min"] > 0.0] = 0.0
+        dsm_dict["e_min"][dsm_dict["e_min"] > 0.0] = 0.0
+        dsm_dict["p_max"][dsm_dict["p_max"] < 0.0] = 0.0
+        dsm_dict["e_max"][dsm_dict["e_max"] < 0.0] = 0.0
+
+    else:
+        with session_scope_egon_data(engine) as session:
+            query = session.query(
+                egon_etrago_electricity_cts_dsm_timeseries.bus.label("site_id"),
+                egon_etrago_electricity_cts_dsm_timeseries.p_min,
+                egon_etrago_electricity_cts_dsm_timeseries.p_max,
+                egon_etrago_electricity_cts_dsm_timeseries.e_min,
+                egon_etrago_electricity_cts_dsm_timeseries.e_max,
+            ).filter(
+                egon_etrago_electricity_cts_dsm_timeseries.scn_name == scenario,
+                egon_etrago_electricity_cts_dsm_timeseries.bus
+                == edisgo_obj.topology.id,
+            )
+            df = pd.read_sql(sql=query.statement, con=engine)
+        # add time step column
+        df["time_step"] = len(df) * [np.arange(0, 8760)]
+        # un-nest time series data and pivot so that time_step becomes index and
+        # site_id column names
+        dsm_dict["p_min"] = _pivot_abs_helper(df, "p_min")
+        dsm_dict["p_max"] = _pivot_abs_helper(df, "p_max")
+        dsm_dict["e_min"] = _pivot_abs_helper(df, "e_min")
+        dsm_dict["e_max"] = _pivot_abs_helper(df, "e_max")
+
+    # distribute over all CTS loads
+    cts_loads = edisgo_obj.topology.loads_df[
+        (edisgo_obj.topology.loads_df.type == "conventional_load")
+        & (edisgo_obj.topology.loads_df.sector == "cts")
+    ]
+    if not dsm_dict["p_min"].empty:
+        if len(cts_loads) == 0:
+            raise ValueError("There is CTS DSM potential but no CTS loads.")
+        for dsm_ts in ["p_min", "p_max", "e_min", "e_max"]:
+            dsm_dict[dsm_ts] = pd.DataFrame(
+                data=(
+                    np.matmul(
+                        dsm_dict[dsm_ts].values, np.matrix(cts_loads["p_set"].values)
+                    )
+                    / cts_loads["p_set"].sum()
+                ),
+                index=dsm_dict[dsm_ts].index,
+                columns=cts_loads["p_set"].index,
+            )
+
+    return dsm_dict
+
+
+def _pivot_pu_helper(df_db, pu_col, scale_col):
+    df = (
+        df_db.loc[:, ["site_id", scale_col, f"{pu_col}_pu", "time_step"]]
+        .explode([f"{pu_col}_pu", "time_step"])
+        .astype({f"{pu_col}_pu": "float"})
+    )
+    df[pu_col] = df[f"{pu_col}_pu"] * df[scale_col]
+    df = df.pivot(index="time_step", columns="site_id", values=pu_col)
+    return df
+
+
+def _pivot_abs_helper(df_db, col):
+    df = (
+        df_db.loc[:, ["site_id", col, "time_step"]]
+        .explode([col, "time_step"])
+        .astype({col: "float"})
+    )
+    df = df.pivot(index="time_step", columns="site_id", values=col)
+    return df
