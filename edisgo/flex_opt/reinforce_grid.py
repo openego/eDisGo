9dd4784edcaccff465e83a44bb4e22e458ccff40
Maike Held <Maike.Held@rl-institut.de>
2023-04-03 11:51:04 +0200

    Minor fix in reinforce_grid.py

--- edisgo/flex_opt/reinforce_grid.py
+++ edisgo/flex_opt/reinforce_grid.py
@@ -367,7 +367,9 @@ def reinforce_grid(
 
     # check if all voltage problems were solved after maximum number of
     # iterations allowed
-    if while_counter == max_while_iterations and crit_nodes:
+    if (
+        while_counter == max_while_iterations and not crit_nodes.empty
+    ):  # ToDo: ist das hier richtig?
         edisgo_reinforce.results.unresolved_issues = pd.concat(
             [
                 edisgo_reinforce.results.unresolved_issues,
