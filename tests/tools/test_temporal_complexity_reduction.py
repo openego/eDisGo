4aff553ff5a77db84d845ef91aecf532f905c35e
birgits <birgit.schachler@rl-institut.de>
2024-01-18 20:55:31 -0800

    Fix test - setup class needs to be rerun

--- tests/tools/test_temporal_complexity_reduction.py
+++ tests/tools/test_temporal_complexity_reduction.py
@@ -7,7 +7,7 @@ from edisgo.tools import temporal_complexity_reduction as temp_red
 
 
 class TestTemporalComplexityReduction:
-    @classmethod
+    @pytest.fixture(autouse=True)
     def setup_class(self):
         self.edisgo = EDisGo(ding0_grid=pytest.ding0_test_network_path)
         self.edisgo.set_time_series_worst_case_analysis()
