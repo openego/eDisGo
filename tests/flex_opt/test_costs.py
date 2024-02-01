Fix test as costs calculation changed

tests/flex_opt/test_costs.py
@@ -97,8 +97,8 @@ class TestCosts:
         assert costs.loc["Line_10019", "type"] == "48-AL1/8-ST1A"
         assert costs.loc["Line_10019", "voltage_level"] == "mv"
         assert np.isclose(costs.loc["Line_50000002", "total_costs"], 2.34)
-        assert np.isclose(costs.loc["Line_50000002", "length"], 0.09)
-        assert costs.loc["Line_50000002", "quantity"] == 3
+        assert np.isclose(costs.loc["Line_50000002", "length"], 0.03)
+        assert costs.loc["Line_50000002", "quantity"] == 1
         assert costs.loc["Line_50000002", "type"] == "NAYY 4x1x35"
         assert costs.loc["Line_50000002", "voltage_level"] == "lv"
 