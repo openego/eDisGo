d68e4d4c609bcc36f0fbd4ed49526107f527a6e7
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2024-08-12 13:51:08 +0200

    small bug fix in topology connect_to_lv

--- edisgo/network/topology.py
+++ edisgo/network/topology.py
@@ -2289,7 +2289,9 @@ class Topology:
                     "component is therefore connected to random LV bus."
                 )
                 bus = random.choice(
-                    lv_grid.buses_df[~lv_grid.buses_df.in_building.astype(bool)].index
+                    lv_grid.buses_df[
+                        ~lv_grid.buses_df.in_building.astype(bool)
+                    ].index.tolist()
                 )
                 comp_data.pop("geom", None)
                 comp_data.pop("p")
