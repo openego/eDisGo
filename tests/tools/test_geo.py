d490810bf6903f192ae8b3bfdb8571aac3b609f7
birgits <birgit.schachler@rl-institut.de>
2024-09-13 11:16:41 +0200

    Add test for find_nearest_bus

---
+++ tests/tools/test_geo.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+
+from shapely.geometry import Point
+
+from edisgo import EDisGo
+from edisgo.tools import geo
+
+
+class TestTools:
+    @classmethod
+    def setup_class(self):
+        self.edisgo = EDisGo(
+            ding0_grid=pytest.ding0_test_network_3_path, legacy_ding0_grids=False
+        )
+
+    def test_find_nearest_bus(self):
+        # test with coordinates of existing bus
+        bus = self.edisgo.topology.buses_df.index[5]
+        point = Point(
+            (
+                self.edisgo.topology.buses_df.at[bus, "x"],
+                self.edisgo.topology.buses_df.at[bus, "y"],
+            )
+        )
+        nearest_bus, dist = geo.find_nearest_bus(point, self.edisgo.topology.buses_df)
+        assert nearest_bus == bus
+        assert dist == 0.0
+
+        # test with random coordinates
+        point = Point((10.002736, 47.5426))
+        nearest_bus, dist = geo.find_nearest_bus(point, self.edisgo.topology.buses_df)
+        assert nearest_bus == "BranchTee_mvgd_33535_lvgd_1163360000_building_431698"
+        assert np.isclose(dist, 0.0008622, atol=1e-6)
