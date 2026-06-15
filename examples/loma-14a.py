4c32b0e995355c20843ff3edd7d6e803da214c97
CarlosEpia <caepiar@unal.edu.co>
2026-06-15 11:56:30 +0200

    include voltage offset 2.5 percent

--- examples/loma-14a.py
+++ examples/loma-14a.py
@@ -345,6 +345,10 @@ def main():
     #edisgo = EDisGo(pypsa_csv_dir=grid_path, snapshot_range=(2159, 2327)) #first week april 2025
     #edisgo = EDisGo(pypsa_csv_dir=grid_path, snapshot_range=(5088, 5256)) #first week august 2025
 
+    # Set HV/MV transformer secondary voltage to 1.025 pu to reflect the common
+    # DSO practice of boosting LV bus voltage to compensate for feeder voltage drops.
+    edisgo.config["grid_expansion_allowed_voltage_deviations"]["hv_mv_trafo_offset"] = 0.025
+
     mv_grid_geom = gpd.read_file(path_husum_district_shp).to_crs(4326)
     edisgo.topology.grid_district["geom"] = mv_grid_geom.loc[0, "geometry"]
     edisgo.topology.grid_district["srid"] = 4326
